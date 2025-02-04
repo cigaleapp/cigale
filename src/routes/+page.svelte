351189bc78feb7bc787dcc99df88b423c074ba08
Gwen Le Bihan <hey@ewen.works>
2025-02-04 16:11:53 +0100

    ✨ Import tensorflow PoC from @tianoc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to chocolat</h1>
+<input type="file" accept="image/*" bind:files={image_file}/>
+<p>classse : {classe} with certainty : {certainty}</p>
+<canvas id="canvas" bind:this={canva_element}></canvas>
+<script>
+    import * as tf from "@tensorflow/tfjs";
+    import * as mobilenet from "@tensorflow-models/mobilenet";
+	import { abs } from "@tensorflow/tfjs";
+    let image_file = $state();
+    let classe = $state();
+    let certainty = $state();
+
+    let canva_element = $state();
+
+
+    $effect(() => {
+        if (image_file) {
+            let img = Array.from(image_file)[0];
+            let reader = new FileReader();
+            reader.onload = function (e) {
+                let img = new Image();
+                img.src = e.target.result;
+                img.onload = async function () {
+                    let tensor = tf.browser.fromPixels(img).resizeBilinear([224, 224]).toFloat();
+                    tensor = tensor.div(tensor.max());
+                    tensor = tensor.sub(tensor.mean())
+                    //tensor = tensor.sub([0.485, 0.456, 0.406]).div([0.229, 0.224, 0.225]);
+
+                    let tensor_copy = tf.browser.fromPixels(img).clone();
+
+                    tensor_copy = tensor_copy.resizeBilinear([224, 224]).toFloat();
+                    tensor_copy = tensor_copy.div(255.0);
+
+                    
+                    canva_element.width = 224;
+                    canva_element.height = 224;
+                    await tf.browser.toPixels(tensor_copy, canva_element);
+
+                    tensor = tensor.expandDims()
+
+                    const model = await mobilenet.load();
+                    const predictions = await model.classify(img);
+                    
+                    console.log(predictions);
+                    classe = predictions[0].className;
+                    certainty = predictions[0].probability;
+                }
+            }
+            reader.readAsDataURL(img);
+
+        }
+    })
+
+</script>
