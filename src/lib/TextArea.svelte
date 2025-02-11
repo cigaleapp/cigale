03c0b769d71cdf84fe120aca64dc2e1e6032476c
Gwen Le Bihan <hey@ewen.works>
2025-02-11 17:26:14 +0100

    ✨ Add TextArea

---
+++ src/lib/TextArea.svelte
@@ -0,0 +1,25 @@
+<script>
+	/** @type {{value: string}} */
+	let { value } = $props();
+</script>
+
+<textarea bind:value></textarea>
+
+<style>
+    textarea {
+        font-family: sans-serif;
+        border-radius: var(--corner-radius);
+        border: var(--border-thickness) solid currentColor;
+        outline: none;
+        padding: 0.5em;
+    }
+
+    textarea:hover {
+        background-color: azure;
+    }
+
+    textarea:focus-visible {
+        border-color: chocolate;
+        background-color: azure;
+    }
+</style>
