6d49734bee9e3cb0057f276cca62dd126aa0bf4a
Gwen Le Bihan <hey@ewen.works>
2025-02-11 18:31:26 +0100

    💚 Add /_components index page to go to a component
    since in static builds goign to /_components/Name directly won't work,
    it has to go thru client-side nav first

---
+++ src/routes/_components/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	let componentName = $state('');
+</script>
+
+<input type="text" bind:value={componentName} />
+
+<a href="/_components/{componentName}">Aller à /_components/{componentName}</a>
