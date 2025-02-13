fb0b10109f8211a532c27822198e99fb16b7074c
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-02-13 21:58:27 +0100

    📝 Document TextArea props

--- src/lib/TextArea.svelte
+++ src/lib/TextArea.svelte
@@ -1,26 +1,32 @@
 <script>
-	/** @type {{value: string}} */
-	let { value } = $props();
+	/**
+	 * @typedef Props
+	 * @type {object}
+	 * @property {string} value  the text entered by the user
+	 */
+
+	/** @type {Props} */
+	let { value = $bindable() } = $props();
 </script>
 
 <textarea bind:value></textarea>
 
 <style>
-    textarea {
-        font-family: sans-serif;
-        border-radius: var(--corner-radius);
-        border: var(--border-thickness) solid currentColor;
-        outline: none;
-        padding: 0.5em;
-    }
+	textarea {
+		font-family: sans-serif;
+		border-radius: var(--corner-radius);
+		border: var(--border-thickness) solid currentColor;
+		outline: none;
+		padding: 0.5em;
+	}
 
-    textarea:hover {
-        background-color: var(--bg-primary-translucent);
-    }
+	textarea:hover {
+		background-color: var(--bg-primary-translucent);
+	}
 
-    textarea:focus-visible {
-        border-color: var(--bg-primary);
-        background-color: var(--bg-primary-translucent);
-        color: var(--fg-primary);
-    }
+	textarea:focus-visible {
+		border-color: var(--bg-primary);
+		background-color: var(--bg-primary-translucent);
+		color: var(--fg-primary);
+	}
 </style>
