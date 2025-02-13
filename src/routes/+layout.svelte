add254c5f99fc441dfa8118513654246382845c2
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-02-14 00:10:33 +0100

    ✨ Add toasts system (closes #23)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import { base } from '$app/paths';
 	import { setContext } from 'svelte';
+	import { toasts } from '$lib/toasts.svelte';
+	import Toast from '$lib/Toast.svelte';
+
 	import './style.css';
 
 	const { children } = $props();
@@ -13,12 +16,34 @@
 	<base href={base} />
 </svelte:head>
 
+<section class="toasts">
+	{#each toasts.items as toast (toast.id)}
+		<Toast
+			{...toast}
+			action={toast.labels.action}
+			dismiss={toast.labels.close}
+			onaction={() => {
+				toast.callbacks?.action?.(toast);
+			}}
+			ondismiss={() => {
+				toasts.remove(toast.id);
+			}}
+		/>
+	{/each}
+</section>
+
 {@render children?.()}
 
 <style>
-	:root {
-		--corner-radius: 7px;
-		--border-thickness: 1px;
+	.toasts {
+		position: fixed;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1em;
+		bottom: 1em;
+		left: 0;
+		right: 0;
 	}
 
 	:global(*) {
