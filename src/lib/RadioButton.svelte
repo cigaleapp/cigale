9dfa5bd4737417648cdb7eef7a90cb4c5df7baac
WhiSp00n <gaetanlaumonier493@gmail.com>
2025-02-13 10:04:25 +0100

    🍱 Button bind:group add
    Oui c'est add c'est fou

--- src/lib/RadioButton.svelte
+++ src/lib/RadioButton.svelte
@@ -1,14 +1,16 @@
 <script>
-	let { options, name } = $props()
+	let { options, name } = $props();
+	let resultat = $state(1);
 </script>
 
 <br />
 {#each options as option}
 	<label>
-		<input type="radio" {name} value={option} />
+		<input type="radio" {name} value={option} bind:group={resultat} />
 		{option}
 	</label><br />
 {/each}
+Vous avez choisi l'option {resultat} félicitation
 
 <style>
 	:root {
