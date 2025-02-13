8bc2c0349a91f9c6b386975209c905ce3c898ecd
Ines Charles <ines.charles@etu.toulouse-inp.fr>
2025-02-13 19:57:27 +0000

    🔧 Setup icones
    Closes #21 and #15

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import icons from 'unplugin-icons/vite';
 
 // feur
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		icons({
+			compiler: 'svelte',
+			defaultClass: 'icon'
+		}),
+		sveltekit()
+	]
 });
