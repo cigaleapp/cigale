fcf4b5c6e8ead1a2204c05490328a834070b6084
WhiSp00n <gaetanlaumonier493@gmail.com>
2025-02-14 15:53:25 +0100

    🍱 Cécentrépaskegwennaditquifoksassoicentré

--- src/lib/RadioButton.svelte
+++ src/lib/RadioButton.svelte
@@ -2,12 +2,11 @@
 	let { options, name, value = $bindable() } = $props();
 </script>
 
-<br />
 {#each options as option}
-	<label>
-		<input type="radio" {name} value={option} bind:group={value} />
+	<div class="radio">
+		<input type="radio" {name} value={option} style="align-items: center;" bind:group={value} />
 		{option}
-	</label>
+	</div>
 {/each}
 <br />
 Vous avez choisi l'option {value} félicitation
@@ -15,17 +14,21 @@ Vous avez choisi l'option {value} félicitation
 <style>
 	:root {
 		--accent-color: CornflowerBlue;
-		--color: var(--bg-primary); /*à remplacer avec --bg-primary */
+		--color: var(--bg-primary);
 	}
 
 	input[type='radio'] {
 		accent-color: var(--color);
 		color: --bg-neutral;
-		display: flex;
+		align-items: center;
 	}
 	input[type='radio']:hover {
 		color: var(--color);
 		border-radius: 50%;
 		box-shadow: 0 0 8px var(--color);
+	}
+
+	.radio {
+		align-items: center;
 	}
 </style>
