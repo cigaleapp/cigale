bb8806425fd23830ca9eb9f4a5ce0efad4237819
Lamothe <lamotho>
2025-02-17 14:45:26 +0100

    ✨ InkButton
    closses 19 hopefully (personne n'y croit)

---
+++ src/lib/ButtonInk.svelte
@@ -0,0 +1,32 @@
+<script>
+	function nothing() {}
+
+	let { label, handleclick = nothing } = $props();
+</script>
+
+<button onclick={handleclick}>
+	{label}
+</button>
+
+<style>
+	button {
+		background-color: var(--bg-neutral);
+		color: var(--bg-primary);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: 1px var(--bg-neutral) solid;
+		--height: 1em;
+		--padding: 0.5em;
+		padding: var(--padding);
+		height: calc(var(--height) +2 * var(--padding));
+		border-radius: var(--corner-radius);
+		text-transform: uppercase;
+		font-weight: bold;
+		letter-spacing: 0.05em;
+	}
+
+	button:hover {
+		background-color: var(--bg-primary-translucent);
+	}
+</style>
