36a461f403d79ca873f036af442167b1fc0f2fc8
Lamothe <lamotho>
2025-02-18 11:33:19 +0100

    💄 C'est plus beau comme ça d'abord
    hehe

--- src/lib/Checkbox.svelte
+++ src/lib/Checkbox.svelte
@@ -2,7 +2,7 @@
 <script>
 	//export let value = false;
 	import Icon from '~icons/ph/check-light';
-	let { text, value = $bindable() } = $props();
+	let { children, value = $bindable() } = $props();
 </script>
 
 <label class:checked={value}>
@@ -11,33 +11,35 @@
 		{#if value}
 			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 				<rect
-					x="0"
-					y="0"
-					width="24"
-					height="24"
+					x="1"
+					y="1"
+					width="22"
+					height="22"
 					fill="var(--bg-primary)"
 					stroke="var(--fg-neutral)"
-					stroke-width="2"
+					stroke-width="1"
+					rx="4"
 				/>
 			</svg>
 			<Icon width="20" height="20" style="position: absolute;  color: var(--fg-primary);" />
 		{:else}
 			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 				<rect
-					x="0"
-					y="0"
-					width="24"
-					height="24"
+					x="1"
+					y="1"
+					width="22"
+					height="22"
 					fill="none"
 					stroke="var(--fg-neutral)"
-					stroke-width="2"
+					stroke-width="1"
+					rx="4"
 				/>
 			</svg>
 		{/if}
 	</div>
 
 	<input type="checkbox" bind:checked={value} />
-	{text}
+	{@render children()}
 </label>
 
 <style>
