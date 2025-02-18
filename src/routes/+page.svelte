7fe8fca15b4d54182418e1749b046d4c5af8aee5
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-02-18 01:05:00 +0100

    ✨ Implement multiselect through drag or keyboard shorcuts
    Closes #27

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-	import * as tf from '@tensorflow/tfjs';
+	import AreaObservations from '$lib/AreaObservations.svelte';
 	import * as mobilenet from '@tensorflow-models/mobilenet';
-	import CardObservation from '$lib/CardObservation.svelte';
+	import * as tf from '@tensorflow/tfjs';
 	let image_file = $state();
 	let classe = $state();
 	let certainty = $state();
@@ -29,6 +29,7 @@
 					canva_element.height = 224;
 					await tf.browser.toPixels(tensor_copy, canva_element);
 
+					// eslint-disable-next-line no-unused-vars
 					tensor = tensor.expandDims();
 
 					const model = await mobilenet.load();
@@ -48,15 +49,18 @@
 		'https://www.mnhn.fr/system/files/styles/medium/private/2023-05/Phasme.jpg.webp?itok=ShPjddHh',
 		'https://c02.purpledshub.com/uploads/sites/62/2023/10/What-are-insects.jpg?w=1029&webp=1',
 		'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBOmE9xQpSvkASuRBHnw43Nz6fGcM9P0Ly9Q&s',
+		'https://www.dgaae.de/files/user-upload/insekt_des_jahres/2024/Stierhornk%C3%A4fer_Schmitt.jpg',
+		'https://parlonssciences.ca/sites/default/files/2019-11/What_is_an_insect.jpg',
+		'https://www.mnhn.fr/system/files/styles/medium/private/2023-05/Phasme.jpg.webp?itok=ShPjddHh',
+		'https://c02.purpledshub.com/uploads/sites/62/2023/10/What-are-insects.jpg?w=1029&webp=1',
+		'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBOmE9xQpSvkASuRBHnw43Nz6fGcM9P0Ly9Q&s',
 		'https://www.dgaae.de/files/user-upload/insekt_des_jahres/2024/Stierhornk%C3%A4fer_Schmitt.jpg'
 	].map((image) => ({
 		image,
 		title: `IMG_${Math.ceil(Math.random() * 100000)}.JPEG`,
-		stacksize: Math.ceil(Math.random() * 5)
+		stacksize: Math.random() > 0.2 ? Math.ceil(Math.random() * 5) : 1,
+		loading: Math.random() > 0.8 ? (Math.random() > 0.3 ? Math.random() : -1) : undefined
 	}));
-
-	/** @type {string[]} */
-	let selection = $state([]);
 </script>
 
 <h1>Welcome to chocolat</h1>
@@ -64,25 +68,4 @@
 <p>classse : {classe} with certainty : {certainty}</p>
 <canvas id="canvas" bind:this={canva_element}></canvas>
 
-<section class="images">
-	{#each images as props}
-		<CardObservation
-			{...props}
-			selected={selection.includes(props.title)}
-			onclick={() => {
-				if (selection.includes(props.title)) {
-					selection = selection.filter((title) => title !== props.title);
-				} else {
-					selection.push(props.title);
-				}
-			}}
-		/>
-	{/each}
-</section>
-
-<style>
-	section.images {
-		display: flex;
-		gap: 2em;
-	}
-</style>
+<AreaObservations {images} loadingText="Analyse…" />
