2f5e1420b8c75a914aef0d2c591326321e1b7715
WhiSp00n <gaetanlaumonier493@gmail.com>
2025-02-18 15:33:46 +0100

    🍱 Olivier ?
    En fait on a remarqué que le boutton secondaire d'olivier ça y ressemblait bcp modulo le display:inline-block

---
+++ src/lib/ButtonIcon.svelte
@@ -0,0 +1,29 @@
+<script>
+	let { children } = $props();
+</script>
+
+<button>
+	{@render children()}
+</button>
+
+<style>
+	button {
+		cursor: pointer;
+		background-color: var(--bg-neutral);
+		color: var(--fg-neutral);
+		display: inline-block;
+		justify-content: center;
+		align-items: center;
+		border: 1px solid var(--gray);
+		padding: 0.75em;
+		border-radius: var(--corner-radius);
+		font-weight: bold;
+		font-size: var(--font-size, 1em);
+	}
+
+	button:is(:hover, :focus-visible) {
+		background-color: var(--bg-primary-translucent);
+		color: var(--fg-primary);
+		border-color: var(--bg-primary);
+	}
+</style>
