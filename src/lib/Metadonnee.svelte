626cddb547828de9e3ce61d43b961aa1dfa84d82
Lamothe <lamotho>
2025-02-18 17:50:58 +0100

    ✨ Una metadona
    closes #10 parce que on y croit trop voila si tu lis ça envoie le a 3 de tes amis sinon tu vas mourr dns lé tris prochan jou

---
+++ src/lib/Metadonnee.svelte
@@ -0,0 +1,52 @@
+<script>
+	let { value = $bindable() } = $props();
+</script>
+
+<div class="meta">
+	<div class="label">Label</div>
+	<textarea class="zone" bind:value placeholder="Entrez une valeur"></textarea>
+	<rect class="ligne"></rect>
+</div>
+
+<style>
+	.meta {
+		gap: 0.1em;
+	}
+
+	.label {
+		color: var(--gray);
+		text-transform: uppercase;
+		font-size: 0.75em;
+	}
+
+	.ligne {
+		height: 2px;
+		width: 100px;
+		background-color: var(--bg-neutral);
+		display: flex;
+	}
+
+	.zone {
+		width: 100px;
+		height: 1em;
+		border: none;
+		resize: none;
+	}
+
+	.zone:focus-visible {
+		outline: none;
+		color: var(--bg-primary);
+	}
+
+	.zone:focus-visible::placeholder {
+		color: var(--gray);
+	}
+
+	.meta:hover .ligne {
+		background-color: var(--fg-neutral);
+	}
+
+	.meta:focus-within .ligne {
+		background-color: var(--bg-primary);
+	}
+</style>
