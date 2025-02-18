4ea9e198aa5309a74485dd08d48f979c18d72980
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-02-18 01:29:29 +0100

    🍱 Diversify example image sources

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,12 +49,15 @@
 		'https://www.mnhn.fr/system/files/styles/medium/private/2023-05/Phasme.jpg.webp?itok=ShPjddHh',
 		'https://c02.purpledshub.com/uploads/sites/62/2023/10/What-are-insects.jpg?w=1029&webp=1',
 		'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBOmE9xQpSvkASuRBHnw43Nz6fGcM9P0Ly9Q&s',
+		'https://i.imgur.com/oH8fyj6.png',
 		'https://www.dgaae.de/files/user-upload/insekt_des_jahres/2024/Stierhornk%C3%A4fer_Schmitt.jpg',
-		'https://parlonssciences.ca/sites/default/files/2019-11/What_is_an_insect.jpg',
-		'https://www.mnhn.fr/system/files/styles/medium/private/2023-05/Phasme.jpg.webp?itok=ShPjddHh',
-		'https://c02.purpledshub.com/uploads/sites/62/2023/10/What-are-insects.jpg?w=1029&webp=1',
-		'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBOmE9xQpSvkASuRBHnw43Nz6fGcM9P0Ly9Q&s',
-		'https://www.dgaae.de/files/user-upload/insekt_des_jahres/2024/Stierhornk%C3%A4fer_Schmitt.jpg'
+		'https://www.coastalmountains.org/wp-content/uploads/2023/09/bug1.jpg',
+		'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy4ENS-jTKY1misqIH__jY0fl7Z2Z3r0D_uA&s',
+		'https://i1.sndcdn.com/artworks-60T5acZkPilLZTnc-qOoVZw-t500x500.jpg',
+		'https://www.terro.com/media/Articles/TERRO/How-to-ID-Insects.jpg',
+		'https://i.pinimg.com/originals/d4/b6/c4/d4b6c4366f33a5e45694ccfa466c09f8.jpg',
+		'https://cdn.pixabay.com/photo/2023/04/25/03/02/butterfly-7949342_640.jpg',
+		'https://www.bioexplorer.net/images/1-Blue-Morpho.jpg'
 	].map((image) => ({
 		image,
 		title: `IMG_${Math.ceil(Math.random() * 100000)}.JPEG`,
