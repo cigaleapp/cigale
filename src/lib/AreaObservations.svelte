30ed1cb3b98fdb33725ad7c62cfa2aef73e4d21b
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-02-18 01:35:45 +0100

    🐛 Prevent drag-selection from working outside of images container
    So that we don't have weird selection stuff elsewhere in the UI

--- src/lib/AreaObservations.svelte
+++ src/lib/AreaObservations.svelte
@@ -37,6 +37,7 @@
 		return new DragSelect({
 			// @ts-ignore
 			selectables: [...imagesContainer.querySelectorAll('[data-selectable]')],
+			area: imagesContainer,
 			draggability: false
 		});
 	});
