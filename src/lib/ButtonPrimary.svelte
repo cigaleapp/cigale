31ead357a378f7eb6786d42e0b31d6f54a3b906c
Lamothe <lamotho>
2025-02-18 10:57:02 +0100

    ✨ Closes #4 normalement
    je vous jure y'a une différence entre ça et le boutton secondary

---
+++ src/lib/ButtonPrimary.svelte
@@ -0,0 +1,29 @@
+<script>
+	let { children, onclick } = $props();
+</script>
+
+<button {onclick}>
+	{@render children()}
+</button>
+
+<style>
+	button {
+		cursor: pointer;
+		background-color: var(--bg-primary);
+		color: var(--fg-primary);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: none;
+		padding: 0.75em;
+		border-radius: var(--corner-radius);
+		width: var(--width, 7.5em);
+		font-weight: bold;
+		font-size: var(--font-size, 1em);
+	}
+
+	button:is(:hover, :focus-visible) {
+		background-color: var(--bg-dark-primary);
+		color: var(--fg-dark-primary);
+	}
+</style>
