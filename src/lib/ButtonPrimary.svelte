e84b44eab2c230b153810a5d7337bb07d3b9743c
Gwenn Le Bihan <gwenn.lebihan@etu.inp-n7.fr>
2025-02-19 14:15:36 +0000

    ✨ Implement modal

---
+++ src/lib/ButtonPrimary.svelte
@@ -0,0 +1,16 @@
+<script>
+	import ButtonSecondary from './ButtonSecondary.svelte';
+
+	/** @type {import ('./ButtonSecondary.svelte').Props} */
+	const { children, ...props } = $props();
+</script>
+
+<ButtonSecondary
+	{...props}
+	--bg="var(--bg-primary)"
+	--fg="var(--fg-primary)"
+	--bg-hover="var(--bg-dark-primary)"
+	--fg-hover="var(--fg-dark-primary)"
+>
+	{@render children()}
+</ButtonSecondary>
