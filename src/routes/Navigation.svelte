97556ab66bbb9a39e30ccee07e94045ee12c60ed
Lamothe <lamotho>
2025-02-25 11:06:31 +0100

    ✨ C'est le début de la barre de navigation
    on ne passe pas le barreau: Hînè la génie

---
+++ src/routes/Navigation.svelte
@@ -0,0 +1,57 @@
+<script>
+	import Sup from '~icons/ph/greater-than';
+	import Download from '~icons/ph/download-simple';
+	import logo from '../favicon.png';
+</script>
+
+<div class="navigation">
+	<div class="divLogo">
+		<img class="logo" src={logo} alt="logo" />
+		C.i.g.a.l.e.
+	</div>
+
+	<p>Import</p>
+
+	<Sup></Sup>
+	<p>Crop top</p>
+
+	<Sup></Sup>
+	<p>Classification</p>
+
+	<Sup></Sup>
+	<div class="download">
+		<Download></Download>
+		<p>Résultats</p>
+	</div>
+</div>
+
+<style>
+	.navigation {
+		background-color: var(--bg-primary-translucent);
+		height: 8%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		padding-left: 10px;
+		padding-right: 50px;
+		resize: vertical;
+	}
+
+	.logo {
+		width: 60px;
+		height: 40px;
+	}
+	.divLogo {
+		display: flex;
+		align-items: center;
+		font-weight: bold;
+		text-transform: uppercase;
+	}
+
+	.download {
+		display: flex;
+		align-items: center;
+		gap: 1em;
+	}
+</style>
