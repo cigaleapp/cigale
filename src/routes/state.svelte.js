49454feca91b5c5a9a90e88c9b86478b90f6fb26
celeste <celeste.tiano@etu.toulouse-inp.fr>
2025-03-04 17:20:08 +0100

    added necessary packages

--- src/routes/state.svelte.js
+++
@@ -1,5 +0,0 @@
-export const img_proceed = $state({
-    nb: 0,
-    time:0,
-    state: "",
-})
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import icons from 'unplugin-icons/vite';
-import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
+import crossOriginIsolation from 'vite-plugin-cross-origin-isolation';
 
 export default defineConfig({
 	plugins: [
@@ -9,15 +9,12 @@ export default defineConfig({
 			compiler: 'svelte',
 			defaultClass: 'icon'
 		}),
-		sveltekit()
+		sveltekit(),
+		crossOriginIsolation()
 	],
-		// This is a known issue when using WebAssembly with Vite
-	// Need to specify `optimizeDeps.exclude` to NPM packages that uses WebAssembly
-	// See: https://github.com/vitejs/vite/issues/8427
 	optimizeDeps: {
 		exclude: ['onnxruntime-web']
 	},
 
 	assetsInclude: ['**/*.wasm']
 });
-
