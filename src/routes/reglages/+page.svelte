1f6fc0003f38ed84fcd5e60c7103ff25b6745937
WhiSp00n <gaetanlaumonier493@gmail.com>
2025-03-04 17:04:39 +0100

    🍱 Bdd + reglages Ok mais pas modification
    noice

---
+++ src/routes/reglages/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+	import { tables } from '$lib/idb';
+	import SelectWithSearch from '$lib/SelectWithSearch.svelte';
+</script>
+
+<h1>C'est le réglages des protocooooooooooooooles</h1>
+<p>On peut changer les paramètres des protocoles ici</p>
+
+<div>
+	{#await tables.Protocol.list()}
+		<p>C'est beau la vie hein ?</p>
+	{:then protocols}
+		<ul style="list-style-type: none;">
+			{#each protocols as protocol (protocol.id)}
+				<li>
+					<div class="protoCard">
+						<ul style="list-style-type: none;">
+							<li>Nom du protocole : {protocol.name}</li>
+							<li>Id : {protocol.id}</li>
+							<li>Source : {protocol.source}</li>
+
+							<li>Metadonnées :</li>
+							<SelectWithSearch options={protocol.metadata} searchQuery="" selectedValue="" />
+							<li>Email de l'auteur.ice : {protocol.author.email}</li>
+							<li>Nom de l'auteur.ice : {protocol.author.name}</li>
+						</ul>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	{:catch error}
+		<p>Erreur : {error}</p>
+	{/await}
+</div>
+
+<style>
+	h1 {
+		padding-top: 40px;
+		color: var(--fg-primary);
+	}
+	.protoCard {
+		display: flex;
+		flex-direction: column;
+		background-color: var(--bg-primary-translucent);
+		padding: 10px;
+		margin: 10px;
+		border-radius: 10px;
+		width: fit-content;
+		font-weight: bold;
+	}
+	ul {
+		list-style-position: inside;
+		padding-left: 0;
+	}
+</style>
