02cb3d34267fea6b21e1d84d5b2985a7e27794d9
Gaetan Laumonier <gaetan.laumonier@etu.inp-n7.fr>
2025-03-07 09:55:19 +0000

    ✨ Réglages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -98,4 +98,21 @@
 	:global(*) {
 		font-family: 'Host Grotesk', sans-serif;
 	}
+
+	.navbar {
+		position: fixed;
+		z-index: 100;
+		width: 100%;
+		height: fit-content;
+	}
+
+	.container {
+		margin-left: auto;
+		margin-right: 0;
+		display: flex;
+		flex-direction: column;
+		position: fixed;
+		top: 0;
+		z-index: 100;
+	}
 </style>
