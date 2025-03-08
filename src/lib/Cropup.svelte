c8cb936834d16ef87bb0e55345c4f15d5339c32a
celeste <celeste.tiano@etu.toulouse-inp.fr>
2025-03-08 17:11:36 +0100

    heuu le croppage marchouille un epu...

--- src/lib/Cropup.svelte
+++ src/lib/Cropup.svelte
@@ -14,8 +14,8 @@ show a pop up to crop an image
 	 * @property {string} key a unique string, used to identify the modal in the page's state.
 	 * @property {() => void} [opener] a function you can bind to, to open the modal
 	 * @property {string} image the image to crop 
-     * @property {typeof bbs.BoundingBoxes[]} boundingBoxes the bounding boxes to display
-     * @property {(boundingbox:typeof bbs.BoundingBoxes[], id:number) => void} onconfirm a function to call when the user confirms the crop
+     * @property {{x: number, y: number, width: number, height: number}[]} boundingBoxes the bounding boxes to display
+     * @property {(boundingbox:{x: number, y: number, width: number, height: number}, id:number) => void} onconfirm a function to call when the user confirms the crop
      * @property {number} id the id of the image 
 	 */
 
@@ -31,7 +31,7 @@ show a pop up to crop an image
 
     let container = $state();
 
-    /** @type {typeof bbs.BoundingBoxes[]} */
+    /** @type {{x: number, y: number, width: number, height: number}[]} */
     let BBout = [];
     for (let i=0;i<boundingBoxes.length; i++){
         BBout.push({x:boundingBoxes[i].x, y:boundingBoxes[i].y, width:boundingBoxes[i].width, height:boundingBoxes[i].height});
