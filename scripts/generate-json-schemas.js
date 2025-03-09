0a6c07d64284ff5164fd7a288a21d6f114d96b26
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-03-09 16:21:16 +0100

    📝 Mention metaschema version for protocol def json schema
    See https://github.com/arktypeio/arktype/pull/1359

--- scripts/generate-json-schemas.js
+++ scripts/generate-json-schemas.js
@@ -6,7 +6,14 @@ const here = path.dirname(new URL(import.meta.url).pathname);
 const outputDir = path.resolve(here, '../static/');
 
 async function exportJsonSchema(name, schema) {
-	const json = JSON.stringify(schema.in.toJsonSchema(), null, 2);
+	const json = JSON.stringify(
+		{
+			$schema: 'https://json-schema.org/draft-07/schema',
+			...schema.in.toJsonSchema()
+		},
+		null,
+		2
+	);
 	await writeFile(path.resolve(outputDir, `${name}.schema.json`), json);
 }
 
