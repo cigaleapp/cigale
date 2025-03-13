b704eed7509c97c69d5fe840146de70cfcfd3559
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-03-13 22:16:21 +0100

    ✨ Store bounding boxes in YOLO format

--- src/lib/BoundingBoxes.svelte.js
+++ src/lib/BoundingBoxes.svelte.js
@@ -32,3 +32,35 @@ export function toPixelCoords({ x, y, width, height }) {
 		height: height * TARGETHEIGHT
 	};
 }
+
+/**
+ * @param {object} param0
+ * @param {number} param0.x
+ * @param {number} param0.y
+ * @param {number} param0.width
+ * @param {number} param0.height
+ */
+export function toCenteredCoords({ x, y, width, height }) {
+	return {
+		x: x + width / 2,
+		y: y + height / 2,
+		w: width,
+		h: height
+	};
+}
+
+/**
+ * @param {object} param0
+ * @param {number} param0.x
+ * @param {number} param0.y
+ * @param {number} param0.w
+ * @param {number} param0.h
+ */
+export function toTopLeftCoords({ x, y, w, h }) {
+	return {
+		x: x - w / 2,
+		y: y - h / 2,
+		width: w,
+		height: h
+	};
+}
