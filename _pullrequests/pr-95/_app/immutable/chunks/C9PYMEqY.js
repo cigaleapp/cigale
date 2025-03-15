13350c7b9db51c657b078a83b1f6e8a314fa4033
gwennlbh <gwennlbh@users.noreply.github.com>
2025-03-15 20:02:00 +0000

    Deploy preview for PR 95 🛫

---
+++ _pullrequests/pr-95/_app/immutable/chunks/C9PYMEqY.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_1crwz0l)==null?void 0:s.base)??"/cigale/_pullrequests/pr-95";var e;const a=((e=globalThis.__sveltekit_1crwz0l)==null?void 0:e.assets)??l;export{a,l as b};
