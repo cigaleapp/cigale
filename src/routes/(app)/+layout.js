a90974e0c35b23444cc3ce6d825adabd3ca0c02e
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-03-15 02:46:27 +0100

    ✨ Define a real Transect protocol
    See #85

--- src/routes/(app)/+layout.js
+++ src/routes/(app)/+layout.js
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment';
-import { BUILTIN_METADATA } from '$lib/database.js';
+import { BUILTIN_METADATA, BUILTIN_METADATA_IDS } from '$lib/database.js';
 import { error } from '@sveltejs/kit';
 import { openTransaction, tables } from '$lib/idb.svelte.js';
 import { defineSpeciesMetadata } from '$lib/species.svelte.js';
@@ -24,26 +24,28 @@ async function fillBuiltinData() {
 			tx.objectStore('Metadata').put(metadata);
 		}
 		tx.objectStore('Protocol').put({
-			id: 'test',
-			metadata: [...BUILTIN_METADATA.map((m) => m.id), 'species'],
-			authors: [
-				{ name: 'Feur', email: 'gwenn.elbikergre@gmai.com' },
-				{ name: 'Incofeurgniote', email: 'igriuojgr@fokejofe.com' }
+			id: 'io.github.cigaleapp.transect',
+			metadata: [
+				BUILTIN_METADATA_IDS.species,
+				BUILTIN_METADATA_IDS.shoot_date,
+				BUILTIN_METADATA_IDS.shoot_location,
+				BUILTIN_METADATA_IDS.crop
 			],
+			authors: [],
 			exports: {
 				images: {
 					cropped:
-						'{{ default image.metadata.species.valueLabel "(Unknown)" }}/{{ observation.label }}/{{ suffix image.filename "_cropped" }}',
+						'Cropped/{{ default image.metadata.species.valueLabel "(Unknown)" }}_{{ sequence }}.{{ extension image.filename }}',
 					original:
-						'{{ default image.metadata.species.valueLabel "(Unknown)" }}/{{ observation.label }}/{{ suffix image.filename "_original" }}'
+						'Original/{{ default image.metadata.species.valueLabel "(Unknown)" }}_{{ sequence }}.{{ extension image.filename }}'
 				},
 				metadata: {
 					json: 'analysis.json',
 					csv: 'metadata.csv'
 				}
 			},
-			name: 'Test',
-			source: 'https://gwen.works'
+			name: 'Transect',
+			source: 'https://github.com/cigaleapp/cigale'
 		});
 		tx.objectStore('Settings').put({
 			id: 'defaults',
