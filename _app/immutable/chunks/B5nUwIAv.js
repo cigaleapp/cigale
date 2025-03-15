ae1c67cfb71258c68b92d85ee49a15b0f0c59c60
gwennlbh <gwennlbh@users.noreply.github.com>
2025-03-15 14:25:34 +0000

    deploy: 1009f16c74f704d4da3737df6e36410ba1e517d9

---
+++ _app/immutable/chunks/B5nUwIAv.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1e0gt4k)==null?void 0:s.base)??"/cigale";var e;const a=((e=globalThis.__sveltekit_1e0gt4k)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.B4HmR0ok.js
@@ -0,0 +1,1 @@
+import{l as o,b as r}from"../chunks/BI0dUvbG.js";export{o as load_css,r as start};
