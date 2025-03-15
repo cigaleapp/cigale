dda3b704378721a53d324da6996117f3c31284dd
gwennlbh <gwennlbh@users.noreply.github.com>
2025-03-15 12:53:57 +0000

    Deploy preview for PR 82 🛫

---
+++ _pullrequests/pr-82/_app/immutable/chunks/CUlwkU3y.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_15gg441)==null?void 0:s.base)??"/cigale/_pullrequests/pr-82";var e;const t=((e=globalThis.__sveltekit_15gg441)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _pullrequests/pr-82/_app/immutable/entry/start.xNV2Xcvm.js
@@ -0,0 +1,1 @@
+import{l as o,b as r}from"../chunks/hXh6-dnz.js";export{o as load_css,r as start};
