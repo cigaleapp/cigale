c1b86373951f0822dc699041c3eaf5780acc1a0a
gwennlbh <gwennlbh@users.noreply.github.com>
2025-03-15 12:34:49 +0000

    Deploy preview for PR 92 🛫

---
+++ _pullrequests/pr-92/_app/immutable/chunks/C_aJz322.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_19vs7ky)==null?void 0:s.base)??"/cigale/_pullrequests/pr-92";var e;const t=((e=globalThis.__sveltekit_19vs7ky)==null?void 0:e.assets)??a;export{t as a,a as b};
