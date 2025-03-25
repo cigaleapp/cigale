f201dd57d6697c06cdfe047d424a5430c454fd1a
gwennlbh <gwennlbh@users.noreply.github.com>
2025-03-25 17:41:02 +0000

    Remove preview for PR 95 🛬

--- _pullrequests/pr-95/_app/immutable/chunks/CSCIDDpC.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1u27ft7)==null?void 0:s.base)??"/cigale/_pullrequests/pr-95";var e;const a=((e=globalThis.__sveltekit_1u27ft7)==null?void 0:e.assets)??t;export{a,t as b};
