5e680310889fc3fa389fa552416f284e260c8dc1
gwennlbh <hey@ewen.works>
2025-04-07 15:30:42 +0000

    Deploying to gh-pages from @ cigaleapp/cigale@7cbe4d1da714be48500a209d270641c23466b2e7 🚀

---
+++ _app/immutable/chunks/Djrgn3pf.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1jup0k2)==null?void 0:s.base)??"/cigale";var e;const a=((e=globalThis.__sveltekit_1jup0k2)==null?void 0:e.assets)??t;export{a as assets,t as base};
