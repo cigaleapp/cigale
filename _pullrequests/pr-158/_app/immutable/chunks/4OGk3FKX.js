3d2050233b63f286189a2edc98e742b8f9aee4b6
gwennlbh <gwennlbh@users.noreply.github.com>
2025-04-15 14:56:42 +0000

    Remove preview for PR 158 🛬

--- _pullrequests/pr-158/_app/immutable/chunks/4OGk3FKX.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1uiybxj)==null?void 0:s.base)??"/cigale/_pullrequests/pr-158";var e;const l=((e=globalThis.__sveltekit_1uiybxj)==null?void 0:e.assets)??t;export{l as assets,t as base};
