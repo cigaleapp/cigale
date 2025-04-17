23d72d2b08e3027e5d19d64f789a5b0fc36d3481
gwennlbh <hey@ewen.works>
2025-04-17 16:38:09 +0000

    Deploying to gh-pages from @ cigaleapp/cigale@4467638c4c430161bfa27a5073ee10b4a3c5daab 🚀

---
+++ _app/immutable/chunks/DnIOC_Qj.js
@@ -0,0 +1,1 @@
+import{s as t,p as r}from"./Y6Vcp7MK.js";const e={get error(){return r.error},get params(){return r.params},get route(){return r.route},get state(){return r.state},get status(){return r.status},get url(){return r.url}};t.updated.check;const a=e;export{a as p};
