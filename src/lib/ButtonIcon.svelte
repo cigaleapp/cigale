8b9810383a906289700c9e1df09602162bdc9d38
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-04-24 15:53:13 +0200

    🚸 Rework cropper navigation, add continue button

--- src/lib/ButtonIcon.svelte
+++ src/lib/ButtonIcon.svelte
@@ -22,24 +22,22 @@
 <style>
 	button {
 		cursor: pointer;
-		background-color: var(--bg-primary-translucent);
-		color: var(--fg-primary);
+		background-color: var(--bg, transparent);
+		color: var(--fg, var(--fg-primary));
 		display: inline-flex;
 		justify-content: center;
 		align-items: center;
 		border: 1px solid var(--gray);
-		padding: 0.75em;
+		padding: 0.5em;
 		border-radius: var(--corner-radius);
 		font-weight: bold;
 		font-size: var(--font-size, 1em);
 		border: none;
-		height: 40px;
-		width: 40px;
 	}
 
 	button:is(:hover, :focus-visible) {
-		background-color: var(--bg-neutral);
-		color: var(--fg-neutral);
+		background-color: var(--hover-bg, var(--bg-primary-translucent));
+		color: var(--hover-fg, var(--fg-neutral));
 	}
 
 	button:disabled {
