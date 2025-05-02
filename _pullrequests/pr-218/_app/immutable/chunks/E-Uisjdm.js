b7f181b2920b30c8c62a61be9d33ff886b4058c5
gwennlbh <gwennlbh@users.noreply.github.com>
2025-05-02 23:02:18 +0000

    Deploy preview for PR 218 🛫

---
+++ _pullrequests/pr-218/_app/immutable/chunks/E-Uisjdm.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_1l0sbfr)==null?void 0:s.base)??"/cigale/_pullrequests/pr-218";var e;const a=((e=globalThis.__sveltekit_1l0sbfr)==null?void 0:e.assets)??l;export{a,l as b};
