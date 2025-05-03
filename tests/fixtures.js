d4c23551cff60e92df8c058682f225dd4c208e05
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-05-03 19:11:38 +0200

    ✅ Fix IndexedDB setup

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -5,8 +5,17 @@ import { test as base } from '@playwright/test';
  */
 
 /**
- * @type {TestType<PlaywrightTestArgs & PlaywrightTestOptions, PlaywrightWorkerArgs & PlaywrightWorkerOptions>}
+ * @type {TestType<{ forEachTest: void } & PlaywrightTestArgs & PlaywrightTestOptions, PlaywrightWorkerArgs & PlaywrightWorkerOptions>}
  */
-export const test = base.extend({});
+export const test = base.extend({
+	forEachTest: [
+		async ({ page }, use) => {
+			await page.goto('/');
+			await page.waitForFunction(() => Boolean(window.DB));
+			await use();
+		},
+		{ auto: true }
+	]
+});
 
 export { expect } from '@playwright/test';
