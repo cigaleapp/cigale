6cf166126131a25846bbe6c51062b17dde174171
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-05-03 13:43:18 +0200

    💚 Fix playwright "must be object destructuring pattern" for test = base.extend(

--- tests/setup.js
+++ tests/setup.js
@@ -8,7 +8,7 @@ import { test as base } from '@playwright/test';
  * @type {TestType<PlaywrightTestArgs & PlaywrightTestOptions & { db: {name: string; version: number} }, PlaywrightWorkerArgs & PlaywrightWorkerOptions>}
  */
 export const test = base.extend({
-	db: async (_, use) => {
+	db: async ({ page: _page }, use) => {
 		use({ name: 'database', version: 3 });
 	}
 });
