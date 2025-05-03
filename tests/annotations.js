98788a98a8051acce01c5ec8f73672700387c012
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-05-03 22:36:14 +0200

    ✅ Playwright: annotate PR/issue test.fixme's with tags too
    since annotations are sadly not visible for skipped tests, it seems

--- tests/annotations.js
+++ tests/annotations.js
@@ -3,6 +3,7 @@
  */
 export function pr(number) {
 	return {
+		tag: `@issue-${number}`,
 		annotation: {
 			type: 'pullrequest',
 			description: `https://github.com/cigaleapp/cigale/pull/${number}`
@@ -15,6 +16,7 @@ export function pr(number) {
  */
 export function issue(number) {
 	return {
+		tag: `@pr-${number}`,
 		annotation: {
 			type: 'issue',
 			description: `https://github.com/cigaleapp/cigale/issues/${number}`
