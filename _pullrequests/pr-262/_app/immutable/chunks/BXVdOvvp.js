7380e2330c7d2790912142bcc3a6bd115f4067d4
gwennlbh <gwennlbh@users.noreply.github.com>
2025-05-14 14:35:08 +0000

    Remove preview for PR 262 🛬

--- _pullrequests/pr-262/_app/immutable/chunks/BXVdOvvp.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_8vigca)==null?void 0:s.base)??"/cigale/_pullrequests/pr-262";var a;const t=((a=globalThis.__sveltekit_8vigca)==null?void 0:a.assets)??e;export{t as a,e as b};
