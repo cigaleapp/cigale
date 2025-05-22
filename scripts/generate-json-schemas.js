fb42ba4950e701bcdb9324b41d11c6e8997bb405
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-05-22 10:51:32 +0200

    🐛 Remove $ark.object table keys from generate json schemas

--- scripts/generate-json-schemas.js
+++ scripts/generate-json-schemas.js
@@ -1,5 +1,6 @@
 import { writeFile } from 'node:fs/promises';
 import path from 'node:path';
+import { type } from 'arktype';
 import { ExportedProtocol } from '../src/lib/protocols.js';
 import { Analysis } from '../src/lib/schemas/results.js';
 import { Taxonomy } from '../src/lib/taxonomy.js';
@@ -13,14 +14,16 @@ const outputDir = path.resolve(here, '../static/');
  * @param {import('arktype').Type} schema
  */
 async function exportJsonSchema(name, schema) {
-	const json = JSON.stringify(
-		{
+	let schemaObject = {
 			$schema: 'https://json-schema.org/draft-07/schema',
 			...schema.in.toJsonSchema()
-		},
-		null,
-		2
-	);
+	};
+
+	// Remove any key that as a value matching a $ark.object
+	const isArkObject = entry => type(["'table'", /^\$ark\.object\d+$/]).allows(entry)
+	schemaObject = deleteKeys(schemaObject, isArkObject)
+
+	const json = JSON.stringify(schemaObject, null, 2);
 	await writeFile(path.resolve(outputDir, `${name}.schema.json`).replace('C:\\C:\\', 'C:\\'), json);
 }
 
@@ -28,4 +31,12 @@ if (process.argv[1] === import.meta.filename) {
 	await exportJsonSchema('protocol', ExportedProtocol);
 	await exportJsonSchema('taxonomy', Taxonomy);
 	await exportJsonSchema('results', Analysis);
+}
+
+function deleteKeys(obj, pred) {
+	return Object.fromEntries(
+		Object.entries(obj)
+			.filter(entry => pred(entry))
+			.map(entry => deleteKeys(entry, pred))
+	)
 }
