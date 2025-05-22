efb232f9dea708675547efce378a8aebb77524ef
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-05-22 10:55:40 +0200

    🎨 Format scripts/generate-json-schema.js

--- scripts/generate-json-schemas.js
+++ scripts/generate-json-schemas.js
@@ -1,6 +1,6 @@
 import { writeFile } from 'node:fs/promises';
 import path from 'node:path';
-import { type } from 'arktype';
+import { type } from 'arktype';
 import { ExportedProtocol } from '../src/lib/protocols.js';
 import { Analysis } from '../src/lib/schemas/results.js';
 import { Taxonomy } from '../src/lib/taxonomy.js';
@@ -15,13 +15,13 @@ const outputDir = path.resolve(here, '../static/');
  */
 async function exportJsonSchema(name, schema) {
 	let schemaObject = {
-			$schema: 'https://json-schema.org/draft-07/schema',
-			...schema.in.toJsonSchema()
+		$schema: 'https://json-schema.org/draft-07/schema',
+		...schema.in.toJsonSchema()
 	};
 
 	// Remove any key that as a value matching a $ark.object
-	const isArkObject = entry => type(["'table'", /^\$ark\.object\d+$/]).allows(entry)
-	schemaObject = deleteKeys(schemaObject, isArkObject)
+	const isArkObject = (entry) => type(["'table'", /^\$ark\.object\d+$/]).allows(entry);
+	schemaObject = deleteKeys(schemaObject, isArkObject);
 
 	const json = JSON.stringify(schemaObject, null, 2);
 	await writeFile(path.resolve(outputDir, `${name}.schema.json`).replace('C:\\C:\\', 'C:\\'), json);
@@ -36,7 +36,7 @@ if (process.argv[1] === import.meta.filename) {
 function deleteKeys(obj, pred) {
 	return Object.fromEntries(
 		Object.entries(obj)
-			.filter(entry => pred(entry))
-			.map(entry => deleteKeys(entry, pred))
-	)
+			.filter((entry) => pred(entry))
+			.map((entry) => deleteKeys(entry, pred))
+	);
 }
