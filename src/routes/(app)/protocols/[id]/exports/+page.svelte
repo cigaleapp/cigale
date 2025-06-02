6626e6ee568f7f243b4bb5894b0c9ba8ec98e587
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-06-03 00:12:01 +0200

    👷 Protocol edition: exports page

--- src/routes/(app)/protocols/[id]/exports/+page.svelte
+++ src/routes/(app)/protocols/[id]/exports/+page.svelte
@@ -1,6 +1,233 @@
 <script>
 	import { page } from '$app/state';
+	import { tables } from '$lib/idb.svelte.js';
+	import InlineTextInput from '$lib/InlineTextInput.svelte';
 	import { seo } from '$lib/seo.svelte';
+	import { getSettings } from '$lib/settings.svelte';
+	import { toasts } from '$lib/toasts.svelte';
+	import IconZipFile from '~icons/ph/file-archive';
+	import IconJsonFile from '~icons/ph/file-code';
+	import IconCsvFile from '~icons/ph/file-csv';
+	import IconFolder from '~icons/ph/folder';
+	import IconFolderNew from '~icons/ph/folder-simple-dashed';
+	import IconFullImage from '~icons/ph/image';
+	import IconCroppedImage from '~icons/ph/image-square';
+
+	/**
+	 * @import { TreeNode, NodeProvenance } from './utils.js';
+	 */
+
+	/**
+	 *
+	 * @param {NodeProvenance} provenance
+	 * @return {import('svelte').Component}
+	 */
+	const iconOfNode = (provenance) => {
+		switch (provenance) {
+			case 'metadata.csv':
+				return IconCsvFile;
+			case 'metadata.json':
+				return IconJsonFile;
+			case 'images.cropped':
+				return IconCroppedImage;
+			case 'images.original':
+				return IconFullImage;
+		}
+	};
 
 	seo({ title: `Protocole ${page.params.id}: Export` });
+
+	const { data } = $props();
+	const treeNodes = $state(data.initialTree ?? []);
+
+	/**
+	 * @param {NodeProvenance} provenance
+	 * @param {string} path
+	 */
+	async function updateExportsPath(provenance, path) {
+		const [dotpathParent, dotpathChild] = provenance.split('.');
+
+		const { id, exports } = data.protocol;
+		if (!exports) {
+			toasts.error("Aucune configuration d'exports trouvée pour ce protocole.");
+			return;
+		}
+
+		const newExports = {
+			images: {
+				cropped: exports.images.cropped.toJSON(),
+				original: exports.images.original.toJSON()
+			},
+			metadata: {
+				csv: exports.metadata.csv,
+				json: exports.metadata.json
+			}
+		};
+
+		// @ts-expect-error
+		newExports[dotpathParent][dotpathChild] = path.replace(/^\//, '').replace(/\/$/, '');
+
+		await tables.Protocol.update(id, 'exports', newExports);
+	}
+
+	/**
+	 * @param {TreeNode} children
+	 * @param {string} dirname
+	 */
+	async function updateExportPaths(children, dirname) {
+		for (const child of children) {
+			if ('folder' in child) {
+				await updateExportPaths(child.children, `${dirname}/${child.folder}`);
+			} else {
+				await updateExportsPath(child.provenance, `${dirname}/${child.filename}`);
+			}
+		}
+	}
 </script>
+
+{#snippet tree(/** @type {TreeNode} */ children, /** @type {string} */ dirname = '')}
+	<ul class="tree">
+		<li class="new-folder">
+			<button>
+				<IconFolderNew />
+				<InlineTextInput
+					discreet
+					value=""
+					placeholder="Nouveau dossier"
+					label="Nom du dossier à créer"
+					onblur={(folder, setValue) => {
+						if (!folder.trim()) {
+							toasts.error('Le nom du dossier ne peut pas être vide.');
+							return;
+						}
+
+						setValue('');
+						children.push({ folder, children: [] });
+					}}
+				/>
+			</button>
+		</li>
+		{#each children as child ('folder' in child ? child.folder : child.filename)}
+			<li>
+				{#if 'folder' in child}
+					{@const Icon = child.icon ?? IconFolder}
+					<Icon />
+					<div class="text">
+						<InlineTextInput
+							discreet
+							value={child.folder}
+							label="Nom du dossier"
+							onblur={async (newName) => {
+								child.folder = newName;
+								await updateExportPaths(child.children, `${dirname}/${newName}`);
+							}}
+						/>
+					</div>
+				{:else}
+					{@const Icon = iconOfNode(child.provenance)}
+					<Icon />
+					<div class="text">
+						<span class="filename">
+							<InlineTextInput
+								discreet
+								value={child.filename}
+								label="Nom du fichier"
+								onblur={async (newName) => {
+									child.filename = newName;
+									await updateExportsPath(child.provenance, `${dirname}/${newName}`);
+								}}
+							/>
+						</span>
+						<span class="help">{child.help}</span>
+					</div>
+				{/if}
+			</li>
+			{#if 'children' in child}
+				{@render tree(child.children, `${dirname}/${child.folder}`)}
+			{/if}
+		{/each}
+	</ul>
+{/snippet}
+
+<ul class="tree">
+	<li>
+		<IconZipFile />
+		<span class="filename">Résultats.zip</span>
+	</li>
+	{@render tree(treeNodes, '')}
+</ul>
+
+{#if getSettings().showTechnicalMetadata}
+	<pre class="debug">{JSON.stringify(
+			tables.Protocol.state.find((p) => p.id === data.protocol.id)?.exports,
+			null,
+			2
+		)}</pre>
+{/if}
+
+<style>
+	ul {
+		--indent: 2rem;
+		list-style: none;
+		padding-left: var(--indent);
+		padding-top: 0.75rem;
+		gap: 0.75rem;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.text {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		margin-left: 0.5rem;
+	}
+
+	.text .help {
+		font-size: 0.9em;
+		color: var(--gray);
+	}
+
+	ul ul {
+		position: relative;
+	}
+	ul ul::before {
+		content: '';
+		position: absolute;
+		width: 2px;
+		background-color: var(--fg-neutral);
+		/* XXX the -3px is pixel-perfect-fiddling because icons are a little slimmer than their whole bounding width. visual alignement, basically */
+		left: calc(var(--indent) / 2 - 2px - 3px);
+		top: 0;
+		bottom: 0;
+	}
+
+	ul ul li {
+		position: relative;
+	}
+	ul ul li::before {
+		content: '';
+		position: absolute;
+		left: calc(-1 * var(--indent) / 2 - 2px - 3px);
+		width: calc(var(--indent) / 2);
+		height: 2px;
+		background-color: var(--fg-neutral);
+	}
+
+	li,
+	li > button {
+		display: flex;
+		align-items: center;
+		gap: 0.5rem;
+	}
+	button {
+		font-size: 1rem;
+		border: none;
+		padding: 0;
+	}
+
+	pre.debug {
+		margin-top: 3rem;
+		font-size: 0.7em;
+	}
+</style>
