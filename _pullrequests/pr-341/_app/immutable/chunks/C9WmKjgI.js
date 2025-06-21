62d3f00e9f0fdfb91486a19250966605dd2db507
gwennlbh <gwennlbh@users.noreply.github.com>
2025-06-21 07:01:36 +0000

    Deploy preview for PR 341 🛫

---
+++ _pullrequests/pr-341/_app/immutable/chunks/C9WmKjgI.js
@@ -0,0 +1,1 @@
+import{e as a,u as f}from"./BR2tqTLH.js";function o({title:e}){if(a()){document.title=e?`${e} · Cigale`:"Cigale";return}f(()=>{document.title=e?`${e} · Cigale`:"Cigale"})}export{o as s};
