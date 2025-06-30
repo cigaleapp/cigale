2013158beb30a4658877909776e3ca6ef042f23d
gwennlbh <gwennlbh@users.noreply.github.com>
2025-06-30 06:52:35 +0000

    Remove preview for PR 365 🛬

--- _pullrequests/pr-365/_app/immutable/chunks/B_6XRxP8.js
+++
@@ -1,1 +0,0 @@
-var s;const l=((s=globalThis.__sveltekit_1wlu09d)==null?void 0:s.base)??"/cigale/_pullrequests/pr-365";var e;const a=((e=globalThis.__sveltekit_1wlu09d)==null?void 0:e.assets)??l;export{a,l as b};
