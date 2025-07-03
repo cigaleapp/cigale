65ce5ec27b8500fb4a8cad7ae722472224dfbfef
gwennlbh <gwennlbh@users.noreply.github.com>
2025-07-03 12:55:51 +0000

    Remove preview for PR 368 🛬

--- _pullrequests/pr-368/_app/immutable/chunks/DcTQ08rQ.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_3k5mky)==null?void 0:s.base)??"/cigale/_pullrequests/pr-368";var e;const t=((e=globalThis.__sveltekit_3k5mky)==null?void 0:e.assets)??a;export{t as a,a as b};
--- _pullrequests/pr-368/_app/immutable/nodes/6.DL3xoGK1.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"../chunks/o-Z3nGLH.js";export{m as component};
