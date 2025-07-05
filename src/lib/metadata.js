b76eb0fea2281f1c88c9fb768ee9b885490223c0
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-07-05 17:53:00 +0200

    🐛 Fix CSV export not containing valueLabels

--- src/lib/metadata.js
+++ src/lib/metadata.js
@@ -562,8 +562,9 @@ function toNumber(type, values) {
  * Used for e.g. CSV exports.
  * @param {DB.Metadata} metadata the metadata definition
  * @param {DB.MetadataValue['value']} value the value of the metadata
+ * @param {string} [valueLabel] the label of the value, if applicable (e.g. for enums)
  */
-export function metadataPrettyValue(metadata, value) {
+export function metadataPrettyValue(metadata, value, valueLabel = undefined) {
 	switch (metadata.type) {
 		case 'boolean':
 			return value ? 'Oui' : 'Non';
@@ -572,7 +573,7 @@ export function metadataPrettyValue(metadata, value) {
 			return value instanceof Date ? Intl.DateTimeFormat('fr-FR').format(value) : value.toString();
 
 		case 'enum':
-			return metadata.options?.find((o) => o.key === value.toString())?.label ?? value.toString();
+			return valueLabel || value.toString();
 
 		case 'location': {
 			const { latitude, longitude } = type({ latitude: 'number', longitude: 'number' }).assert(
