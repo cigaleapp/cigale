409f06607678ab1fab0ebf963e2e6a6dc94427d2
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-07-14 22:54:33 +0200

    🚸 Add messages to make initial load screen more interesting

--- src/routes/(app)/+layout.js
+++ src/routes/(app)/+layout.js
@@ -8,17 +8,21 @@ import { error } from '@sveltejs/kit';
 import * as Swarpc from 'swarpc';
 
 export async function load() {
+	setLoadingMessage('Chargement du worker neuronal…');
 	const swarpc = Swarpc.Client(PROCEDURES, {
 		worker: new NeuralWorker({ name: 'SWARPC Neural Worker' })
 	});
 
+	setLoadingMessage('Initialisation DB du worker neuronal…');
 	await swarpc.init({
 		databaseName,
 		databaseRevision
 	});
 
 	try {
+		setLoadingMessage('Initialisation de la base de données…');
 		await tables.initialize();
+		setLoadingMessage('Chargement des données intégrées…');
 		await fillBuiltinData();
 		await tables.initialize();
 	} catch (e) {
@@ -32,6 +36,7 @@ export async function load() {
 }
 
 async function fillBuiltinData() {
+	setLoadingMessage('Initialisation des réglages par défaut…');
 	await openTransaction(['Metadata', 'Protocol', 'Settings'], {}, async (tx) => {
 		tx.objectStore('Settings').put({
 			id: 'defaults',
@@ -45,6 +50,7 @@ async function fillBuiltinData() {
 		});
 	});
 
+	setLoadingMessage('Chargement des protocoles intégrés…');
 	// TODO: remove this at some point
 	await tables.Protocol.remove('io.github.cigaleapp.arthropods.transects');
 
@@ -56,7 +62,48 @@ async function fillBuiltinData() {
 				'https://raw.githubusercontent.com/cigaleapp/cigale/main/examples/arthropods.cigaleprotocol.json'
 			)
 				.then((res) => res.text())
-				.then((txt) => importProtocol(txt, { json: true }));
+				.then((txt) =>
+					importProtocol(txt, {
+						json: true,
+						onLoadingState(state, detail) {
+							let secondLine = '';
+							switch (state) {
+								case 'parsing':
+									secondLine = 'Analyse';
+									break;
+
+								case 'filtering-builtin-metadata':
+									secondLine = 'Filtrage des métadonnées intégrées';
+									break;
+
+								case 'input-validation':
+									secondLine = 'Validation';
+									break;
+
+								case 'write-protocol':
+									secondLine = 'Écriture du protocole';
+									break;
+
+								case 'write-metadata':
+									secondLine = `Écriture de la métadonnée ${detail}`;
+									break;
+
+								case 'write-metadata-options':
+									secondLine = `Écriture des options de la métadonnée ${detail}`;
+									break;
+
+								case 'output-validation':
+									secondLine = 'Post-validation';
+									break;
+
+								default:
+									break;
+							}
+
+							setLoadingMessage(`Chargement du protocole intégré<br>${secondLine}`);
+						}
+					})
+				);
 		} catch (error) {
 			console.error(error);
 			toasts.error(
@@ -64,4 +111,12 @@ async function fillBuiltinData() {
 			);
 		}
 	}
+}
+
+/**
+ * @param {string} message
+ */
+function setLoadingMessage(message) {
+	const loadingMessage = document.getElementById('loading-message');
+	if (loadingMessage) loadingMessage.innerHTML = message;
 }
