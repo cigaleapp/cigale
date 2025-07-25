04278264e95a047c684f1463df027ee67439a422
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-07-26 01:51:15 +0200

    🍱 Use real protocol in screenshots

--- tests/readme.spec.js
+++ tests/readme.spec.js
@@ -3,7 +3,7 @@ import { chooseDefaultProtocol, importPhotos, setSettings } from './utils';
 import français from '../messages/fr.json' with { type: 'json' };
 import english from '../messages/en.json' with { type: 'json' };
 
-test.describe('screenshots', () => {
+test.describe('screenshots', { tag: '@real-protocol' }, () => {
 	test.skip(
 		Boolean(process.env.CI),
 		"Skipping these in CI, they're meant for automated README screenshots updates"
@@ -20,6 +20,8 @@ test.describe('screenshots', () => {
 					en: 'English',
 					fr: 'Français'
 				};
+
+				await page.setViewportSize({ width: (16 / 9) * 720, height: 720 });
 
 				await setSettings({ page }, { showTechnicalMetadata: false });
 
@@ -67,6 +69,11 @@ test.describe('screenshots', () => {
 				await page.waitForTimeout(10_000);
 				await page.getByText('lil-fella', { exact: true }).first().click();
 				await page.getByTestId('sidepanel').getByRole('combobox').first().click();
+				await page
+					.getByTestId('metadata-combobox-viewport')
+					.getByRole('option', { name: 'Allacma fusca' })
+					.hover();
+				await page.waitForTimeout(2_000);
 				await expect(page).toHaveScreenshot();
 			});
 		});
