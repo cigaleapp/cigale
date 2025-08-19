5531aa0299601ff81f9e2595fad22011d5435efa
gwennlbh <hey@ewen.works>
2025-08-19 18:31:46 +0000

    Deploying to gh-pages from @ cigaleapp/cigale@7dc1995ba690cfd614f4c6a57f875ffc0132a3be 🚀

---
+++ _app/immutable/chunks/C7KW9imI.js
@@ -0,0 +1,1 @@
+const c="7dc1995ba690cfd614f4c6a57f875ffc0132a3be";export{c as v};
---
+++ _app/immutable/chunks/J_x3eJoB.js
@@ -0,0 +1,1 @@
+import{aD as o}from"./CR7vsbqc.js";import{v as c}from"./C7KW9imI.js";const r=!0,t=r,e=o;export{t as browser,e as dev,c as version};
