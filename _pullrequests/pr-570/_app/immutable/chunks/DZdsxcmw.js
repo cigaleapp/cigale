c26ef9fd674498f091e562951d6edaf52be50a47
gwennlbh <gwennlbh@users.noreply.github.com>
2025-08-22 21:36:59 +0000

    Deploy preview for PR 570 🛫

---
+++ _pullrequests/pr-570/_app/immutable/chunks/DZdsxcmw.js
@@ -0,0 +1,1 @@
+import{m as a,a as f}from"./B4n3kGg-.js";function o({title:e}){if(a()){document.title=e?`${e} · Cigale`:"Cigale";return}f(()=>{document.title=e?`${e} · Cigale`:"Cigale"})}export{o as s};
