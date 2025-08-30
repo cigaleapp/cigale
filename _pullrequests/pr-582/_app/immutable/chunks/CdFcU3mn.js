f14a19003084582aa20c8fe6a6879607bcfb5983
gwennlbh <gwennlbh@users.noreply.github.com>
2025-08-30 00:37:28 +0000

    Deploy preview for PR 582 🛫

---
+++ _pullrequests/pr-582/_app/immutable/chunks/CdFcU3mn.js
@@ -0,0 +1,1 @@
+import{l as a,a as f}from"./BAbTowNM.js";function o({title:e}){if(a()){document.title=e?`${e} · Cigale`:"Cigale";return}f(()=>{document.title=e?`${e} · Cigale`:"Cigale"})}export{o as s};
