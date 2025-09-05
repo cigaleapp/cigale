318834480040f2c2fa8aa868f30cff04632df123
gwennlbh <gwennlbh@users.noreply.github.com>
2025-09-05 11:28:54 +0000

    Deploy preview for PR 607 🛫

---
+++ _pullrequests/pr-607/_app/immutable/chunks/CRyzgyGQ.js
@@ -0,0 +1,1 @@
+import{b as a,c}from"./DyYkI3S3.js";function i({title:e}){if(a()){document.title=e?`${e} · Cigale`:"Cigale";return}c(()=>{document.title=e?`${e} · Cigale`:"Cigale"})}export{i as s};
---
+++ _pullrequests/pr-607/_app/immutable/nodes/1.CLpxOKlz.js
@@ -0,0 +1,1 @@
+import{_ as m}from"../chunks/CO65J02t.js";export{m as component};
