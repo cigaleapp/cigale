1c383d9762aa90ec0bfaff672c4aeedbaafe7913
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-09-11 15:43:37 +0200

    🐛 Fix all monospace text is in italics
    See https://github.com/datalogix/google-fonts-helper/issues/79

--- scripts/download-google-fonts.js
+++ scripts/download-google-fonts.js
@@ -3,23 +3,26 @@ import { download, constructURL } from 'google-fonts-helper';
 import path from 'node:path';
 const here = import.meta.dirname;
 
-await download(
-	constructURL({
-		display: 'swap',
-		families: {
-			'Host Grotesk': {
-				wght: '300..800',
-				ital: '300..800'
-			},
-			'Fragment Mono': {
-				ital: true
-			}
+const url = constructURL({
+	display: 'swap',
+	families: {
+		'Host Grotesk': {
+			wght: '300..800',
+			ital: '300..800'
+		},
+		'Fragment Mono': {
+			ital: true
 		}
-	}),
-	{
-		outputDir: path.join(here, '../static'),
-		fontsDir: 'fonts',
-		stylePath: 'fonts.css',
-		fontsPath: '/fonts'
 	}
-).execute();
+})
+	// See https://github.com/datalogix/google-fonts-helper/issues/79
+	.replace(':ital@1', ':ital@0;1');
+
+console.info('Downloading', url);
+
+await download(url, {
+	outputDir: path.join(here, '../static'),
+	fontsDir: 'fonts',
+	stylePath: 'fonts.css',
+	fontsPath: '/fonts'
+}).execute();
