8e42318b9087c503c7c57e08e508a6de57d696fa
Gwenn Le Bihan <hey@ewen.works>
2025-09-12 12:50:53 +0200

    🐛 Fix href(...)

--- src/lib/paths.js
+++ src/lib/paths.js
@@ -27,5 +27,5 @@ export async function goto(...args) {
 export function href(...args) {
 	// @ts-expect-error
 	// eslint-disable-next-line svelte/no-navigation-without-resolve
-	return '#' + resolve(...args);
+	return '#' + resolve(...args).replace(resolve('/'), '/');
 }
