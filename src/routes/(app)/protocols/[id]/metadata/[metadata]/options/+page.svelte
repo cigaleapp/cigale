6617646a54499267ae246beb2cc08d94d1f6cf00
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-09-19 21:08:23 +0200

    WIP

---
+++ src/routes/(app)/protocols/[id]/metadata/[metadata]/options/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Logo from '$lib/Logo.svelte';
+</script>
+
+<div class="empty">
+	<Logo --size="5rem" variant="empty" />
+	<p>Sélectionnez une option</p>
+</div>
+
+<style>
+	.empty {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+		width: 100%;
+		flex-grow: 1;
+		gap: 1rem;
+	}
+</style>
