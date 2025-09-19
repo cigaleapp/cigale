62e1cb72286eec330530117f3d46b2727c6b6cee
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-09-19 19:50:27 +0200

    🔥 Remove Sentry (#662)

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,14 +1,4 @@
-import { sequence } from '@sveltejs/kit/hooks';
-import * as Sentry from '@sentry/sveltekit';
 import { paraglideMiddleware } from '$lib/paraglide/server.js';
-
-if (import.meta.env.sentry === 'enabled') {
-	Sentry.init({
-		dsn: 'https://37bb5c00dde50e2bc0dd0d4243db086d@o4509894523551744.ingest.de.sentry.io/4509894524731472',
-		tracesSampleRate: 1,
-		enableLogs: true
-	});
-}
 
 /** @type {import('@sveltejs/kit').Handle} */
 const paraglideHandle = ({ event, resolve }) =>
@@ -30,5 +20,4 @@ const paraglideHandle = ({ event, resolve }) =>
 		});
 	});
 
-export const handle = sequence(Sentry.sentryHandle(), paraglideHandle);
-export const handleError = Sentry.handleErrorWithSentry();
+export const handle = paraglideHandle;
