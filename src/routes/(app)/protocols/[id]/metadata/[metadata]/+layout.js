1fe25d7eaf2f5d04b6a1594cc8fabd68902a34c3
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-09-19 21:08:23 +0200

    WIP

---
+++ src/routes/(app)/protocols/[id]/metadata/[metadata]/+layout.js
@@ -0,0 +1,15 @@
+import { tables } from '$lib/idb.svelte.js';
+import { namespacedMetadataId } from '$lib/schemas/metadata.js';
+import { error } from '@sveltejs/kit';
+
+export async function load({ params, parent }) {
+	const protocol = await parent();
+	const id = namespacedMetadataId(protocol.id, params.metadata);
+	const metadata = await tables.Metadata.get(id);
+	if (!metadata) error(404, `Metadata ${id} for protocol ${protocol.id} not found`);
+
+	return {
+		protocol,
+		metadata
+	};
+}
