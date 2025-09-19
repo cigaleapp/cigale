7614201b56b28bb4c26c634c3cb145c19fe2a2fe
gwennlbh <gwennlbh@users.noreply.github.com>
2025-09-19 18:20:55 +0000

    Deploy preview for PR 299 🛫

---
+++ _pullrequests/pr-299/_app/immutable/chunks/DjGp8dQ-.js
@@ -0,0 +1,1 @@
+import{e as f,k as t}from"./BEJekQTT.js";function o({title:e}){c(()=>{document.title=e?`${e} · Cigale`:"Cigale"})}function c(e){if(f()){e();return}t(()=>{e()})}export{o as s};
---
+++ _pullrequests/pr-299/_app/immutable/nodes/1.Lh24HXYv.js
@@ -0,0 +1,1 @@
+import{_ as m}from"../chunks/D62bTDIu.js";export{m as component};
