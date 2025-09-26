6c5c2a87456a1065fd250ac80e050a0e926f5baa
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-09-26 22:46:36 +0200

    🚧 Fix state updates, implement undo-able removal of metadata & options, improve metadata option creation

--- src/routes/(app)/protocols/[id]/metadata/[metadata]/options/+layout.js
+++ src/routes/(app)/protocols/[id]/metadata/[metadata]/options/+layout.js
@@ -1,10 +1,13 @@
 import { list } from '$lib/idb.svelte.js';
 import { metadataOptionsKeyRange } from '$lib/metadata.js';
+import { namespacedMetadataId } from '$lib/schemas/metadata.js';
 
-export async function load({ params, parent }) {
+export async function load({ params, parent, depends }) {
 	const parentData = await parent();
 
 	const options = await list('MetadataOption', metadataOptionsKeyRange(params.id, params.metadata));
+
+	depends(`idb://Metadata/${namespacedMetadataId(params.id, params.metadata)}/options`);
 
 	return { ...parentData, options };
 }
