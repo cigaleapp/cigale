2420aa7c488cbee63ac566d3c9aca152bd74f88a
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-10-11 15:50:37 +0200

    ♻️ Move MathML to external library
    See https://github.com/gwennlbh/svelte-mathml

--- src/lib/mathml/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Atom } from './Atom.svelte';
-export { default as Fraction } from './Fraction.svelte';
-export { default as Line } from './Line.svelte';
-export { default as Operation } from './Operation.svelte';
-export { default as Subtract } from './Subtract.svelte';
-export { default as Add } from './Add.svelte';
-export { default as Scripts } from './Scripts.svelte';
-export { default as Vector } from './Vector.svelte';
-export { default as Wrap } from './Wrap.svelte';
