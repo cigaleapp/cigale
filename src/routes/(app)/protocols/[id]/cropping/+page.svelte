5ca99bbae4732890eb6b46d9e0e4a58b291da5c2
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-10-12 18:49:14 +0200

    ✨ Add default crop padding setting

--- src/routes/(app)/protocols/[id]/cropping/+page.svelte
+++ src/routes/(app)/protocols/[id]/cropping/+page.svelte
@@ -1,28 +1,32 @@
 <script>
 	import '$lib/fonts-math/import.css';
 
-	import IconDatatype from '$lib/IconDatatype.svelte';
+	import { page } from '$app/state';
 	import ButtonIcon from '$lib/ButtonIcon.svelte';
-	import IconGoto from '~icons/ph/arrow-right';
-	import IconError from '~icons/ph/warning-circle';
 	import ButtonInk from '$lib/ButtonInk.svelte';
 	import Field from '$lib/Field.svelte';
 	import FieldUrl from '$lib/FieldURL.svelte';
+	import IconDatatype from '$lib/IconDatatype.svelte';
+	import { tables } from '$lib/idb.svelte';
 	import InlineTextInput from '$lib/InlineTextInput.svelte';
-	import { fade } from 'svelte/transition';
-	import IconOpenDetails from '~icons/ph/caret-down';
-	import IconAdd from '~icons/ph/plus';
-	import IconTrash from '~icons/ph/trash';
-	import { updater } from '../updater.svelte';
-	import ModelOutputShapeDiagram from './ModelOutputShapeDiagram.svelte';
+	import { goto } from '$lib/paths';
 	import RadioButtons from '$lib/RadioButtons.svelte';
 	import {
 		ensureNamespacedMetadataId,
 		removeNamespaceFromMetadataId
 	} from '$lib/schemas/metadata';
-	import { goto } from '$lib/paths';
-	import { page } from '$app/state';
-	import { tables } from '$lib/idb.svelte';
+	import { Protocol } from '$lib/schemas/protocols';
+	import { toasts } from '$lib/toasts.svelte';
+	import { fade } from 'svelte/transition';
+	import IconGoto from '~icons/ph/arrow-right';
+	import IconOpenDetails from '~icons/ph/caret-down';
+	import IconAdd from '~icons/ph/plus';
+	import IconTrash from '~icons/ph/trash';
+	import IconError from '~icons/ph/warning-circle';
+	import { updater } from '../updater.svelte';
+	import ModelOutputShapeDiagram from './ModelOutputShapeDiagram.svelte';
+	import { TraversalError } from 'arktype';
+	import { errorMessage } from '$lib/i18n';
 
 	const { data } = $props();
 	let settings = $derived(data.crop);
@@ -36,10 +40,7 @@
 	const cropMetadata = $derived(findMetadata(settings.metadata));
 	const cropConfirmationMetadata = $derived(findMetadata(settings.confirmationMetadata));
 
-	$inspect({
-		boxes: [settings.metadata, findMetadata(settings.metadata)],
-		confirmations: [settings.confirmationMetadata, findMetadata(settings.confirmationMetadata)]
-	});
+	let paddingInputError = $state('');
 
 	/**
 	 * @param {number} currentLength
@@ -62,6 +63,35 @@
 
 <div class="content" in:fade={{ duration: 100 }}>
 	<h2>Recadrage des images</h2>
+
+	<Field error={paddingInputError}>
+		{#snippet label()}
+			Marge par défault
+			<p>Pour les images recadrées</p>
+		{/snippet}
+
+		<InlineTextInput
+			label="Marge par défaut"
+			value={settings.padding}
+			Type={Protocol.get('crop', 'padding')}
+			onerror={(msg) => {
+				if (msg instanceof TraversalError) {
+					paddingInputError = msg.arkErrors.map((err) => err.message).join(', ');
+				} else {
+					paddingInputError = errorMessage(msg);
+				}
+			}}
+			onblur={updater((p, value) => {
+				p.crop.padding = value;
+				paddingInputError = '';
+			})}
+		/>
+
+		{#snippet hint()}
+			Entrez un nombre suivi de <code>%</code> pour une marge relative à la taille de chaque
+			image, ou suivi de <code>px</code> pour une marge en pixels.
+		{/snippet}
+	</Field>
 
 	<Field label="Stockage des boîtes">
 		<div class="metadata-link">
