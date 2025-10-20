ffa2b44808614eada808492b83e2e2076575402c
Gwenn Le Bihan <hey@ewen.works>
2025-10-20 20:20:16 +0200

    💚 Adjust mouse coords for crop padding test

--- tests/export.spec.js
+++ tests/export.spec.js
@@ -22,8 +22,8 @@ test('correctly applies crop padding', issue(463), async ({ page }) => {
 	await firstObservationCard(page).click();
 	await page.getByRole('button', { name: "Choisir l'outil 2 points" }).click();
 	// TODO fix coordinates for WebKit, current snapshots is off-center
-	await page.mouse.click(334 - 56, 292 - 38);
-	await page.mouse.click(543 - 56, 501 - 38);
+	await page.mouse.click(278, 255);
+	await page.mouse.click(487, 464);
 
 	await page.locator('nav').getByRole('button', { name: 'Résultats' }).click();
 
