b965e9bf132336330fdaa433c57d6b459fd55d50
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-11-27 23:02:03 +0100

    🍱 Rewrite Jessica Joachim script (#824)

--- scripts/README.md
+++ scripts/README.md
@@ -5,7 +5,7 @@ Scripts pour diverses tâches du projet, notamment la génération de la défini
 Généré ainsi:
 
 1. [`generate-protocol.js`](./generate-protocol.js): Génération du protocole de base, la plupart des métadonnées ont des listes d'options vides. La liste des espèces est initialisée depuis le _classmapping_ du réseau neuronal de classification. Toutes les descriptions, images, liens etc. sont vides.
-2. [`add-species-from-jessica-joachim.js`](./add-species-from-jessica-joachim.js): Ajout de descriptions, images et liens pour les espèces depuis [le site de Jessica Joachim](https://jessica-joachim.com)
+2. [`add-species-from-jessica-joachim.ts`](./add-species-from-jessica-joachim.ts): Ajout de descriptions, images et liens pour les espèces depuis [le site de Jessica Joachim](https://jessica-joachim.com)
 3. [`add-species-from-google-slides.js`](./add-species-from-google-slides.js): Ajout de descriptions, images, liens et potentiellement de nouvelles espèces, depuis des Google Slides d'un dossier Google Drive partagé avec des collaborateurices.
 4. [`add-taxonomic-parents-from-gbif.js`](./add-taxonomic-parents-from-gbif.js): Ajout des données hiérarchiques pour l'inférence taxonomique, et remplissage des listes d'options pour les métadonnées de clades supérieures (ordre, famille, genre, etc) depuis [GBIF](https://gbif.org/). Il remplit aussi les liens, descriptions et images pour les espèces qui n'en aurait toujours pas eu après les étapes précédentes.
 
---
+++ scripts/add-species-from-jessica-joachim.ts
@@ -0,0 +1,341 @@
+import { exists, readFile, writeFile } from 'node:fs/promises';
+import path from 'node:path';
+import { formatDuration, intervalToDuration } from 'date-fns';
+import { JSDOM } from 'jsdom';
+import * as jsdom from 'jsdom';
+import RSSParser from 'rss-parser';
+import Turndown from 'turndown';
+
+import protocol from '../examples/arthropods.cigaleprotocol.json' with { type: 'json' };
+import type { MetadataEnumVariant } from '../src/lib/schemas/metadata';
+import type { ExportedProtocol } from '../src/lib/schemas/protocols';
+
+const LINKS_TO_AVOID = [
+	// FIXME redirects to an image file
+	'https://jessica-joachim.com/insectes/dipteres/syrphidae/paragus-pecchiolii/',
+	// FIXME 404 not found
+	'https://jessica-joachim.com/insectes/dipteres/syrphidae/eupeodes-luniger/'
+];
+
+const cleanedTextContent = (node: Element) =>
+	'textContent' in node && typeof node.textContent === 'string'
+		? node.textContent.replaceAll('\r?\n', ' ').trim()
+		: undefined;
+
+// Suppress annoying virtual console error we can't do anything about and don't care about
+const virtualConsole = new jsdom.VirtualConsole();
+virtualConsole.on('error', (e) => {
+	if (/Could not parse CSS stylesheet/i.test(e.toString())) return;
+	console.error(e);
+});
+
+const here = import.meta.dirname;
+const protocolPath = path.join(here, '../examples/arthropods.cigaleprotocol.json');
+const rss = new RSSParser();
+const tdown = new Turndown();
+let speciesLinks: Map<string, URL> | undefined;
+// For some reason Turndown#turndown is typed as string instead of a method returning string
+const htmlToMarkdown = (html: string) =>
+	(tdown.turndown as unknown as (input: string) => string)(html);
+
+const ORIGIN = 'https://jessica-joachim.com';
+
+if (import.meta.main) {
+	await main();
+}
+
+async function main() {
+	const augmented = await augmentProtocol(protocol);
+	await Bun.write(protocolPath, JSON.stringify(augmented, null, 2));
+	await Bun.$`bunx prettier --write ${protocolPath}`;
+}
+
+async function augmentProtocol(
+	protocol: typeof ExportedProtocol.infer,
+	limit = -1
+): Promise<typeof ExportedProtocol.infer> {
+	const augmented = structuredClone(protocol);
+	const protocolSpecies =
+		augmented.metadata['io.github.cigaleapp.arthropods.example__species'].options!;
+
+	let total = 0;
+	let done = 0;
+	let processed = 0;
+	const eta = new EtaCalculator(10);
+
+	total = protocolSpecies.length;
+	for (const s of protocolSpecies) {
+		done++;
+
+		if (limit > 0 && done > limit) break;
+
+		const pageUrl = await searchForSpecies(s.label);
+		if (!pageUrl) {
+			// console.warn(yellow(`⁄ Could not find page for species ${s.label}`));
+			continue;
+		}
+
+		const newData = await getSpecies(s.key, pageUrl);
+
+		if (!newData) {
+			console.warn(yellow(`⁄ Could not extract data for species ${s.label}`));
+			continue;
+		}
+
+		Object.assign(s, newData);
+
+		processed++;
+
+		const stepTookMs = eta.msSinceLastStep();
+		eta.step();
+
+		console.info(
+			// FIXME use percentage advancement instead of 1_700
+			`${align(processed, total)} ${percentage(done, total, 1)} ${cyan(`→ ${eta.display(1_700 - processed)}`)} Updating species ${dim(`took ${stepTookMs.toFixed(0)}ms`)} ${s.label}  with ${pageUrl} `
+		);
+	}
+
+	console.info(
+		cyan(
+			`⁄ Finished processing species, updated ${processed}/${total} entries (${percentage(processed, total)})`
+		)
+	);
+
+	return augmented;
+}
+
+async function searchForSpecies(name: string): Promise<URL | null> {
+	speciesLinks ??= await fetchAndParseHtml(new URL(`/identification`, ORIGIN)).then(
+		(doc) =>
+			new Map(
+				[...(doc?.getElementById('Listefiches')?.querySelectorAll('a') ?? [])]
+					.map((a) => {
+						const text = cleanedTextContent(a) ?? '';
+						if (!text) return null;
+						const url = new URL(a.href, ORIGIN);
+						if (!url) return null;
+						return [text, url];
+					})
+					.filter((e): e is [string, URL] => e !== null)
+			)
+	);
+
+	for (const [title, link] of speciesLinks ?? []) {
+		if (LINKS_TO_AVOID.includes(link.href)) {
+			continue;
+		}
+		if (blogTitleMatchesSpeciesName(title, name)) {
+			return link;
+		}
+	}
+
+	return null;
+}
+
+async function getSpecies(
+	key: string,
+	page: URL
+): Promise<Partial<typeof MetadataEnumVariant.infer>> {
+	const doc = await fetchAndParseHtml(page);
+	if (!doc) throw new Error(`Could not fetch or parse page at ${page.toString()}`);
+
+	const content = doc.querySelector('main');
+	if (!content) throw new Error(`Could not find main content in page at ${page.toString()}`);
+
+	const imageUrl = content.querySelector('img')?.src;
+
+    const removeAll = (selector: string) => content.querySelectorAll(selector).forEach((el) => el.remove());
+
+	// First image, keeping it in description would be redundant
+	content.querySelector('img')?.remove();
+	// Comments
+	content.querySelector('.entry-content > #comments')?.remove();
+	// Like button section
+	content.querySelector('.entry-content > [id^=like-post-wrapper]')?.remove();
+	// Social media share buttons
+	content.querySelector('.entry-content > .sharedaddy')?.remove();
+	// Text (most likely header) for the removed gallery
+	removeAll('.entry-content > p:has(+ .wp-block-kadence-advancedgallery)');
+	// Gallery (can't be represented in markdown well)
+	removeAll('.entry-content > .wp-block-kadence-advancedgallery');
+	removeAll('.entry-content > .tiled-gallery');
+	removeAll('.entry-content > [id^=gallery-]');
+	removeAll('.entry-content > [id^=gallery-]');
+	// Navigation buttons
+	removeAll('.entry-content > .wp-block-kadence-advancedbtn');
+	// Remove title
+	content.querySelector('h1')?.remove();
+	// Remove year-only bold text, it's usually above a gallery (that is now removed)
+	content.querySelectorAll('strong, h1, h2, h3, h4, h5, h6').forEach((node) => {
+		const text = cleanedTextContent(node) ?? '';
+
+		if (/^20\d\d(-20\d\d)?$/.test(text)) {
+			node.remove();
+		}
+	});
+
+	// TODO improve this
+	content.querySelectorAll('img').forEach((img) => img.remove());
+
+	// Remove links that became empty
+	content.querySelectorAll('a').forEach((node) => {
+		const text = cleanedTextContent(node) ?? '';
+
+		if (!text) {
+			node.remove();
+		}
+	});
+
+	return {
+		key,
+		learnMore: page.toString(),
+		description: htmlToMarkdown(content.innerHTML).trim(),
+		image: imageUrl
+	};
+}
+
+// TODO remove once TS declares it itself
+declare global {
+	interface RegExpConstructor {
+		escape(s: string): string;
+	}
+}
+
+function blogTitleMatchesSpeciesName(title: string, name: string): boolean {
+	const normalize = (str: string) => str.trim().toLowerCase();
+
+	const candidates = [
+		new RegExp(`^${RegExp.escape(name)}$`, 'i'),
+		new RegExp(`^.+ \\(${RegExp.escape(name)}\\)$`, 'i'),
+		new RegExp(`^${RegExp.escape(name)} \\(.+\\)$`, 'i')
+	];
+
+	for (const candidate of candidates) {
+		if (candidate.test(normalize(title))) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+// ANSI control sequences
+class CC {
+	static clearline = '\x1b[2K';
+	static red = '\x1b[31m';
+	static reset = '\x1b[0m';
+	static dim = '\x1b[2m';
+	static blue = '\x1b[34m';
+	static bold = '\x1b[1m';
+	static yellow = '\x1b[33m';
+	static cyan = '\x1b[36m';
+}
+
+function yellow(text: string): string {
+	return `${CC.yellow}${text}${CC.reset}`;
+}
+
+function cyan(text: string): string {
+	return `${CC.cyan}${text}${CC.reset}`;
+}
+
+function dim(text: string): string {
+	return `${CC.dim}${text}${CC.reset}`;
+}
+
+function percentage(part: number, total: number, precision = 0): string {
+	return ((part / total) * 100).toFixed(precision).padStart(3 + precision, ' ') + '%';
+}
+
+function align<T extends string | number>(num: T, total: T | T[]): string {
+	return num.toString().padStart(total.toString().length);
+}
+
+class EtaCalculator {
+	private lastSteps: number[] = [];
+	private maxSteps: number;
+	private lastStepTime: number = performance.now();
+
+	constructor(maxSteps: number) {
+		this.maxSteps = maxSteps;
+	}
+
+	step(): void {
+		const currentTime = performance.now();
+		const durationSeconds = (currentTime - this.lastStepTime) / 1000;
+		this.lastStepTime = currentTime;
+		this.addStep(durationSeconds);
+	}
+
+	msSinceLastStep(): number {
+		return performance.now() - this.lastStepTime;
+	}
+
+	private addStep(durationSeconds: number) {
+		this.lastSteps.push(durationSeconds);
+		if (this.lastSteps.length > this.maxSteps) {
+			this.lastSteps.shift();
+		}
+	}
+
+	private getAverage(): number {
+		const sum = this.lastSteps.reduce((a, b) => a + b, 0);
+		return sum / this.lastSteps.length;
+	}
+
+	seconds(remainingSteps: number): number {
+		return this.getAverage() * remainingSteps;
+	}
+
+	display(remainingSteps: number): string {
+		const {
+			hours = 0,
+			minutes = 0,
+			seconds = 0
+		} = intervalToDuration({
+			start: 0,
+			end: this.seconds(remainingSteps) * 1000
+		});
+
+		const formatPart = (n: number) => n.toString().padStart(2, '0');
+
+		if (hours === 0) {
+			return `${formatPart(minutes)}m${formatPart(seconds)}`;
+		}
+
+		return `${formatPart(hours)}h${formatPart(minutes)}`;
+	}
+}
+
+async function fetchAndParseHtml(url: URL | string): Promise<JSDOM['window']['document']> {
+	const cachedir = path.join('.jessica-joachim-cache');
+	const cachefile = path.join(
+		cachedir,
+		encodeURIComponent(url.toString().replace(ORIGIN, '')) + '.html'
+	);
+
+	let html: string;
+
+	if (await exists(cachefile)) {
+		html = await readFile(cachefile, 'utf-8');
+	} else {
+		const response = await fetch(url);
+		if (!response.ok) {
+			if (response.status === 429) {
+				await new Promise((resolve) => setTimeout(resolve, 10_000));
+				return fetchAndParseHtml(url);
+			}
+			throw new Error(
+				`Failed to fetch ${url.toString()} : ${response.status} ${response.statusText}`
+			);
+		}
+
+		html = await response.text();
+
+		if (await exists(cachedir)) {
+			await writeFile(cachefile, html);
+		}
+	}
+
+	return new JSDOM(html, { virtualConsole }).window.document;
+}
