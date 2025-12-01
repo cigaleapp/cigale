8ceeb08e05455fd8764dcfe9baea2644490e52c0
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-12-01 16:31:06 +0100

    ✅ E2E-test metadata combobox search via synonym

--- tests/sidepanel.spec.js
+++ tests/sidepanel.spec.js
@@ -109,28 +109,40 @@ test('can update a enum-type metadata with cascades', async ({ page }) => {
 	await expect.soft(page.getByTestId('metadata-combobox-viewport')).toMatchAriaSnapshot(`
 	  - option /Entomobrya muscorum \\d+%/ [selected]:
 	    - img
+	    - text: ""
 	    - code: /\\d+%/
 	  - option /Dicyrtomina saundersi \\d+%/:
+	    - text: ""
 	    - code: /\\d+%/
 	  - option "Allacma fusca 9%":
+	    - text: ""
 	    - code: 9%
 	  - option "Pogonognathellus longicornis 5%":
+	    - text: ""
 	    - code: 5%
 	  - option "Dicyrtomina ornata 4%":
+	    - text: ""
 	    - code: 4%
 	  - option "Orchesella quinquefasciata 3%":
+	    - text: ""
 	    - code: 3%
 	  - option "Entomobrya nivalis 2%":
+	    - text: ""
 	    - code: 2%
 	  - option "Entomobrya superba 2%":
+	    - text: ""
 	    - code: 2%
 	  - option "Caprainea marginata 2%":
+	    - text: ""
 	    - code: 2%
 	  - heading "Entomobrya muscorum" [level=2]
 	  - link "En savoir plus gbif.org":
 	    - /url: https://gbif.org/species/2120749
 	    - img
+	    - text: ""
 	    - code: gbif.org
+	  - paragraph:
+	    - emphasis: Métadonées mise à jour à la sélection de cette option
 	  - table:
 	    - rowgroup:
 	      - row "Règne Animalia":
@@ -151,8 +163,6 @@ test('can update a enum-type metadata with cascades', async ({ page }) => {
 	      - row "└─ Genre Entomobrya":
 	        - cell "└─ Genre"
 	        - cell "Entomobrya"
-	  - paragraph:
-	    - emphasis: Métadonées mise à jour à la sélection de cette option
 	`);
 
 	// Hovering over another option
@@ -163,28 +173,40 @@ test('can update a enum-type metadata with cascades', async ({ page }) => {
 	await expect.soft(page.getByTestId('metadata-combobox-viewport')).toMatchAriaSnapshot(`
 	  - option /Entomobrya muscorum \\d+%/ [selected]:
 	    - img
+	    - text: ""
 	    - code: /\\d+%/
 	  - option /Dicyrtomina saundersi \\d+%/:
+	    - text: ""
 	    - code: /\\d+%/
 	  - option "Allacma fusca 9%":
+	    - text: ""
 	    - code: 9%
 	  - option "Pogonognathellus longicornis 5%":
+	    - text: ""
 	    - code: 5%
 	  - option "Dicyrtomina ornata 4%":
+	    - text: ""
 	    - code: 4%
 	  - option "Orchesella quinquefasciata 3%":
+	    - text: ""
 	    - code: 3%
 	  - option "Entomobrya nivalis 2%":
+	    - text: ""
 	    - code: 2%
 	  - option "Entomobrya superba 2%":
+	    - text: ""
 	    - code: 2%
 	  - option "Caprainea marginata 2%":
+	    - text: ""
 	    - code: 2%
 	  - heading "Dicyrtomina saundersi" [level=2]
 	  - link "En savoir plus gbif.org":
 	    - /url: https://gbif.org/species/4536978
 	    - img
+	    - text: ""
 	    - code: gbif.org
+	  - paragraph:
+	    - emphasis: Métadonées mise à jour à la sélection de cette option
 	  - table:
 	    - rowgroup:
 	      - row "Règne Animalia":
@@ -205,8 +227,6 @@ test('can update a enum-type metadata with cascades', async ({ page }) => {
 	      - row "└─ Genre Dicyrtomina":
 	        - cell "└─ Genre"
 	        - cell "Dicyrtomina"
-	  - paragraph:
-	    - emphasis: Métadonées mise à jour à la sélection de cette option
 	`);
 
 	// Selecting the other option
@@ -276,28 +296,38 @@ test('can update a enum-type metadata with cascades', async ({ page }) => {
 	});
 });
 
-test('can search in a enum-type metadata combobox', async ({ page }) => {
-	await initialize({ page });
-	await page.getByTestId('sidepanel').getByRole('combobox').first().fill('Dicyrt');
+test.describe('can search in a enum-type metadata combobox', () => {
+	test('by name', async ({ page }) => {
+		await initialize({ page });
+		await page.getByTestId('sidepanel').getByRole('combobox').first().fill('Dicyrt');
 
-	await expect(page.getByTestId('metadata-combobox-viewport')).toMatchAriaSnapshot(`
+		await expect(page.getByTestId('metadata-combobox-viewport')).toMatchAriaSnapshot(`
 	  - option "Dicyrtoma fusca 0.4%":
+	    - text: ""
 	    - code: 0.4%
 	  - option "Dicyrtomina flavosignata 0.2%":
+	    - text: ""
 	    - code: 0.2%
 	  - option "Dicyrtomina minuta 1%":
+	    - text: ""
 	    - code: 1%
 	  - option "Dicyrtomina ornata 4%":
+	    - text: ""
 	    - code: 4%
 	  - option /Dicyrtomina saundersi \\d+%/:
+	    - text: ""
 	    - code: /\\d+%/
 	  - option "Dicyrtomina signata 0.3%":
+	    - text: ""
 	    - code: 0.3%
 	  - heading "Entomobrya muscorum" [level=2]
 	  - link "En savoir plus gbif.org":
 	    - /url: https://gbif.org/species/2120749
 	    - img
+	    - text: ""
 	    - code: gbif.org
+	  - paragraph:
+	    - emphasis: Métadonées mise à jour à la sélection de cette option
 	  - table:
 	    - rowgroup:
 	      - row "Règne Animalia":
@@ -318,9 +348,61 @@ test('can search in a enum-type metadata combobox', async ({ page }) => {
 	      - row "└─ Genre Entomobrya":
 	        - cell "└─ Genre"
 	        - cell "Entomobrya"
-	  - paragraph:
-	    - emphasis: Métadonées mise à jour à la sélection de cette option
 	`);
+	});
+
+	test('by synonym', async ({ page }) => {
+		await initialize({ page });
+		await page.getByTestId('sidepanel').getByRole('combobox').first().fill('desoria');
+		await expect(page.getByTestId('metadata-combobox-viewport')).toMatchAriaSnapshot(`
+		  - option "Isotoma riparia AKA Desoria riparia 0.4%":
+		    - text: ""
+		    - code: 0.4%
+		  - option "Isotoma viridis AKA Desoria fusia 0.1%":
+		    - text: ""
+		    - code: 0.1%
+		  - option "Isotomurus maculatus AKA Desoria riparia 1%":
+		    - text: ""
+		    - code: 1%
+		  - option "Isotomurus palustris AKA Desoria riparia 0.1%":
+		    - text: ""
+		    - code: 0.1%
+		  - option "Parisotoma notabilis AKA Desoria monticola 0.1%":
+		    - text: ""
+		    - code: 0.1%
+		  - option "Vertagopus asiaticus AKA Desoria cylindrica 0.1%":
+		    - text: ""
+		    - code: 0.1%
+		  - heading "Entomobrya muscorum" [level=2]
+		  - link "En savoir plus gbif.org":
+		    - /url: https://gbif.org/species/2120749
+		    - img
+		    - text: ""
+		    - code: gbif.org
+		  - paragraph:
+		    - emphasis: Métadonées mise à jour à la sélection de cette option
+		  - table:
+		    - rowgroup:
+		      - row "Règne Animalia":
+		        - cell "Règne"
+		        - cell "Animalia"
+		      - row "└─ Phylum Arthropoda":
+		        - cell "└─ Phylum"
+		        - cell "Arthropoda"
+		      - row "└─ Classe Collembola":
+		        - cell "└─ Classe"
+		        - cell "Collembola"
+		      - row "└─ Ordre Entomobryomorpha":
+		        - cell "└─ Ordre"
+		        - cell "Entomobryomorpha"
+		      - row "└─ Famille Entomobryidae":
+		        - cell "└─ Famille"
+		        - cell "Entomobryidae"
+		      - row "└─ Genre Entomobrya":
+		        - cell "└─ Genre"
+		        - cell "Entomobrya"
+		`);
+	});
 });
 
 test('can update a boolean-type metadata', issue(216), async ({ page }) => {
