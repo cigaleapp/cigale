739b9eb179fd169a1683b828c5677ee0855a3c2b
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-12-11 16:08:04 +0100

    💄 Improve disabled state (and transitions from/to disable state) for Button{Secondary,Primary}

--- src/lib/ButtonPrimary.svelte
+++ src/lib/ButtonPrimary.svelte
@@ -11,6 +11,8 @@
 	--fg="var(--fg-primary)"
 	--bg-hover="var(--bg-dark-primary)"
 	--fg-hover="var(--fg-dark-primary)"
+	--bg-disabled="var(--bg-neutral)"
+	--fg-disabled="var(--gray)"
 >
 	{#snippet children(params)}
 		{@render grandchildren(params)}
