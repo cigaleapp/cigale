c286c0ad9ae58024d2c3c734ad4d9c2cb80a3ef5
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-12-12 17:27:10 +0100

    ✏️ Rename strictness metadata label

--- scripts/generate-protocols.js
+++ scripts/generate-protocols.js
@@ -205,7 +205,7 @@ const protocol = {
 		},
 		[namespaced('strictness')]: {
 			type: 'enum',
-			label: "Niveau d'exigence",
+			label: "Exhaustivité du relevé",
 			description: "J'ai photographié",
 			required: true,
 			mergeMethod: 'min',
