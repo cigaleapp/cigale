e669cfac3b0d21911a174dbd24df4d407852b617
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2026-01-23 02:31:44 +0100

    WIP

--- playwright.config.js
+++ playwright.config.js
@@ -8,8 +8,15 @@ import { formatISO9075, minutesToMilliseconds } from 'date-fns';
 /** @typedef {NonNullable<import('@playwright/test').PlaywrightTestConfig['projects']>[number]} Project */
 
 /** @type {Project} */
+const setup = {
+	name: 'setup',
+	testMatch: /setup\/.*\.ts$/
+};
+
+/** @type {Project} */
 const chromium = {
 	name: 'chromium',
+	dependencies: ['setup'],
 	use: {
 		...devices['Desktop Chrome'],
 		contextOptions: {
@@ -21,12 +28,14 @@ const chromium = {
 /** @type {Project} */
 const firefox = {
 	name: 'firefox',
+	dependencies: ['setup'],
 	use: { ...devices['Desktop Firefox'] }
 };
 
 /** @type {Project} */
 const webkit = {
 	name: 'webkit',
+	dependencies: ['setup'],
 	use: {
 		...devices['Desktop Safari'],
 		contextOptions: {
@@ -99,9 +108,9 @@ export default defineConfig({
 
 	/* Configure projects for major browsers */
 	projects: dependsOnTarget({
-		live: [chromium],
-		dev: [chromium, firefox, webkit],
-		built: [chromium, webkit]
+		live: [setup, chromium],
+		dev: [setup, chromium, firefox, webkit],
+		built: [setup, chromium, webkit]
 	}),
 
 	/* Run your local dev server before starting the tests */
--- tests/tasks/dbdump.spec.js
+++ tests/tasks/dbdump.spec.js
@@ -13,34 +13,3 @@ test.skip(
 	Boolean(process.env.CI),
 	'Skipping database dumps and exports on CI, these are meant as easy way to (re)create dump fixtures locally only.'
 );
-
-test('basic', async ({ page }) => {
-	await goToProtocolManagement(page);
-	await importProtocol(page, 'arthropods.light.cigaleprotocol.json');
-
-	await importResults(page, 'exports/correct.zip');
-	await dumpDatabase(page, 'db/basic.devalue');
-});
-
-test('kitchensink-protocol', async ({ page, app }) => {
-	await goToProtocolManagement(page);
-	await importProtocol(page, 'kitchensink.cigaleprotocol.yaml');
-	await page
-		.getByRole('listitem')
-		.filter({ hasText: exampleProtocol.id })
-		.locator('details')
-		.click();
-	await page
-		.getByRole('listitem')
-		.filter({ hasText: exampleProtocol.id })
-		.getByRole('button', { name: 'Supprimer' })
-		.click();
-	await confirmDeletionModal(page, { type: exampleProtocol.name });
-	await expect(page.getByText('Protocole supprimé')).toBeVisible();
-	await newSession(page);
-
-	await app.tabs.go('import');
-	await importPhotos({ page }, 'cyan.jpeg', 'leaf.jpeg');
-	await page.waitForTimeout(2_000);
-	await dumpDatabase(page, 'db/kitchensink-protocol.devalue');
-});
