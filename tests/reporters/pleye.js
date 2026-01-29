cba96c2072895e32cedc7aaa420c1d74b03da88b
gwennlbh <39094199+gwennlbh@users.noreply.github.com>
2026-01-29 20:55:35 +0000

    🎨 Format code [ci skip]

--- tests/reporters/pleye.js
+++ tests/reporters/pleye.js
@@ -5,7 +5,6 @@
 // This script is served by Pleye, on /reporter.js of your instance.
 // Pleye version stptescount update
 
-
 /**
  * @import { Inputs } from '../routes/update/[repository=integer]/inputs';
  * @import * as PW from '@playwright/test/reporter';
@@ -162,7 +161,8 @@ export default class Pleye {
 		}
 
 		this.#stepIndices.set(this.stepIndicesKey(test), -1);
-		if (this.#debugging) console.info('[Pleye] onTestBegin, stepIndices are', this.#stepIndices);
+		if (this.#debugging)
+			console.info('[Pleye] onTestBegin, stepIndices are', this.#stepIndices);
 
 		this.#sendPayload('test-begin', {
 			githubJobId: this.#runData.githubJobId,
@@ -344,7 +344,9 @@ function toError(error) {
  * @returns {string}
  */
 function bufferToText(writes) {
-	return writes.map((chunk) => (Buffer.isBuffer(chunk) ? chunk.toString('utf-8') : chunk)).join('');
+	return writes
+		.map((chunk) => (Buffer.isBuffer(chunk) ? chunk.toString('utf-8') : chunk))
+		.join('');
 }
 
 /**
