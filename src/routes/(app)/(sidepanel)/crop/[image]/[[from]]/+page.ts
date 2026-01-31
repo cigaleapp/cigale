3c20db2dbf82dd072216a707d14303018ca66aa4
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2026-01-31 18:15:13 +0100

    💡 Explain why load function of fullscreen cropper page doesnt depend on DB or route param changes

--- src/routes/(app)/(sidepanel)/crop/[image]/[[from]]/+page.ts
+++ src/routes/(app)/(sidepanel)/crop/[image]/[[from]]/+page.ts
@@ -3,7 +3,20 @@ import { tables } from '$lib/idb.svelte.js';
 import { imageIdToFileId } from '$lib/images';
 import { uiState } from '$lib/state.svelte.js';
 
-export async function load({ params, parent }) {
+// Note: we make sure not to depend on the route parameters or the image data here.
+// This is because the gallery's ordering might change mid-cropping
+// For example:
+// - the crop gallery is groupped by "Confidence in crop metadata",
+// - we manually change a 20%-confidence crop, making it 100%
+//
+// This will change the gallery ordering, bumping the current image to the first group.
+// If this function re-runs, the navigation order will change when we move to the next/previous image,
+// which would be kinda confusing.
+//
+// If we go back to the gallery view though, we quit this route, so going back to the full screen cropper
+// will sort the images again, taking the navigation order change into account once we've seen it in the gallery.
+
+export async function load({ parent }) {
 	// Make sure tables are loaded, otherwise uiState.currentSession will be undefined,
 	// even though uiState.currentSessionId is set.
 	await parent();
@@ -29,6 +42,8 @@ export async function load({ params, parent }) {
 		});
 
 		images.sort((a, b) => imagesSorter(toGalleryItem(a), toGalleryItem(b)));
+
+		console.info(`Sorted ${images.length} images for crop gallery navigation`);
 	}
 
 	return {
