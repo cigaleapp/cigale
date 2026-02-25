16f47422bf7bb66c7d8858c11fa9403a34764428
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2026-02-26 00:08:10 +0100

    ⚡️ Test startup perf on webkit too

--- tests/performance.spec.ts
+++ tests/performance.spec.ts
@@ -4,9 +4,12 @@ import { PerformanceMetricsCollector } from 'playwright-performance-metrics';
 import { assert, expect, test } from './fixtures.js';
 
 test('startup @blank', async ({ page, browserName, app }) => {
-	test.skip(browserName !== 'chromium', 'Performance metrics are only supported in Chromium');
+	let collector: PerformanceMetricsCollector | undefined;
+	if (browserName === 'chromium') {
+		// playwright-performance-metrics only supports Chromium-based browsers
+		collector = new PerformanceMetricsCollector();
+	}
 
-	const collector = new PerformanceMetricsCollector();
 	const start = Date.now();
 
 	await page.goto('./');
@@ -17,13 +20,22 @@ test('startup @blank', async ({ page, browserName, app }) => {
 
 	// TODO: lower this
 	console.info('Startup time:', end - start, 'ms');
-	expect(end - start).toBeLessThan(ms('10s'));
 
-	const metrics = await collector.collectMetrics(page, {
-		timeout: ms('10s')
-	});
+	expect(end - start).toBeLessThan(
+		{
+			chromium: ms('10s'),
+			webkit: ms('15s'),
+			firefox: ms('15s')
+		}[browserName]
+	);
 
-	expect(metrics.largestContentfulPaint).toBeLessThan(ms('5s'));
-	expect(metrics.paint?.firstContentfulPaint).toBeLessThan(ms('250ms'));
-	console.info('Startup performance metrics:', metrics);
+	if (collector) {
+		const metrics = await collector.collectMetrics(page, {
+			timeout: ms('10s')
+		});
+
+		expect(metrics.largestContentfulPaint).toBeLessThan(ms('5s'));
+		expect(metrics.paint?.firstContentfulPaint).toBeLessThan(ms('250ms'));
+		console.info('Startup performance metrics:', metrics);
+	}
 });
