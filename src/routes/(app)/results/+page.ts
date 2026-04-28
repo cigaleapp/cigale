7db0c148cad13b56967a7d75e1e96e636312bd17
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2026-04-28 19:26:34 +0200

    🚸 Redirect to / when trying to access /results without a current session

--- src/routes/(app)/results/+page.ts
+++ src/routes/(app)/results/+page.ts
@@ -1,12 +1,19 @@
+import { redirect } from '@sveltejs/kit';
+
 import { openDatabase } from '$lib/idb.svelte.js';
 import { resolveDefaults } from '$lib/metadata/defaults.js';
 import { uiState } from '$lib/state.svelte.js';
 
 export async function load({ parent }) {
 	await parent();
+	const protocol = uiState.currentProtocol;
+	if (!protocol) {
+		redirect(307, '/');
+	}
+
 	await resolveDefaults({
 		db: await openDatabase(),
-		metadataToConsider: uiState.currentProtocol!.sessionMetadata,
-		sessionId: uiState.currentSession!.id,
+		metadataToConsider: protocol.sessionMetadata,
+		sessionId: protocol.id,
 	});
 }
