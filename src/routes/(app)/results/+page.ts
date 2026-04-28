d029f7c845805617c707adf291edd268d56382bf
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2026-04-28 19:46:23 +0200

    🐛 Fix /results
    See 7db0c148cad13b56967a7d75e1e96e636312bd17 oopsi haha

--- src/routes/(app)/results/+page.ts
+++ src/routes/(app)/results/+page.ts
@@ -7,13 +7,14 @@ import { uiState } from '$lib/state.svelte.js';
 export async function load({ parent }) {
 	await parent();
 	const protocol = uiState.currentProtocol;
-	if (!protocol) {
+	const session = uiState.currentSession;
+	if (!session || !protocol) {
 		redirect(307, '/');
 	}
 
 	await resolveDefaults({
 		db: await openDatabase(),
 		metadataToConsider: protocol.sessionMetadata,
-		sessionId: protocol.id,
+		sessionId: session.id,
 	});
 }
