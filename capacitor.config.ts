7e0bf54dd2ee90717725eb4028b52ebbd1f2305f
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2026-07-04 07:24:34 +0200

    👩‍💻‍ Setup live reload for mobile app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,39 @@
+/// <reference types="@types/node" />
 /// <reference types="@capacitor/splash-screen" />
 /// <reference types="@capgo/capacitor-updater" />
 
+import { networkInterfaces } from 'node:os';
 import type { CapacitorConfig } from '@capacitor/cli';
+
+import arkenv from 'arkenv';
+import { type } from 'arktype';
+
+/**
+ * Put this as your computer's IP in your box's DHCP config
+ * to avoid having to change this file
+ */
+const DEVELOPMENT_COMPUTER_NETWORK_IP = '192.168.1.41';
+
+const env = arkenv({
+	CAPACITOR_LIVE_RELOAD: type.boolean.default(() => {
+		if (process.env.CI) return false;
+
+		// Used to enable CAPACITOR_LIVE_RELOAD
+		// when building/running the app through Android Studio
+		// (annoying to set an env var there)
+		const isLocal = Object.values(networkInterfaces())
+			.flat()
+			.filter((iface) => iface !== undefined)
+			.some(({ address }) => address === DEVELOPMENT_COMPUTER_NETWORK_IP);
+
+		if (isLocal) {
+			console.info(`Building on local dev computer, enabling CAPACITOR_LIVE_RELOAD`);
+			return true;
+		}
+
+		return false;
+	}),
+});
 
 const config: CapacitorConfig = {
 	appId: 'io.github.cigaleapp',
@@ -25,6 +57,12 @@ const config: CapacitorConfig = {
 			signingType: 'apksigner',
 		},
 	},
+	server: env.CAPACITOR_LIVE_RELOAD
+		? {
+				url: `http://${DEVELOPMENT_COMPUTER_NETWORK_IP}:5173`,
+				cleartext: true,
+			}
+		: undefined,
 };
 
 export default config;
